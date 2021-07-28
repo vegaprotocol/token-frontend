ad40909dd66986c88aef11008562a58a1cbf0808
Dexter <dexter.edwards93@gmail.com>
2021-07-28 15:07:46 +0100

    adds ropsten token addresses

--- src/lib/vega-web3.ts
+++ src/lib/vega-web3.ts
@@ -15,6 +15,10 @@ export const Addresses = {
     vestingAddress: "0x23d1bFE8fA50a167816fBD79D7932577c06011f4",
     vegaTokenAddress: "0xcB84d72e61e383767C4DFEb2d8ff7f4FB89abc6e",
   },
+  [EthereumChainIds.Ropsten]: {
+    vestingAddress: "0x08C06ECDCf9b8f45e3cF1ec29B82eFd0171341D9",
+    vegaTokenAddress: "0x16480156222D4525f02F0F2BdF8A45A23bd26431",
+  },
 };
 
 class VegaWeb3 {
