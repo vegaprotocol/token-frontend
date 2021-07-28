18054c2dfc22d83a2e27ae8fc633cf3f2c871c00
Matthew Russell <mattrussell36@gmail.com>
2021-07-27 19:29:12 -0700

    convert data manipulation to use map and added some helper functions

--- src/lib/vega-web3-types.ts
+++ src/lib/vega-web3-types.ts
@@ -5,7 +5,28 @@ export interface Tranche {
   total_added: number
   total_removed: number
   locked_amount: number
-  deposits: Array<any>
-  withdrawals: Array<any>
+  deposits: Array<TrancheDeposit>
+  withdrawals: Array<TrancheWithdrawal>
   users: Array<any>
+}
+
+export interface TrancheDeposit {
+	amount: number
+  user: string
+  tx: string
+}
+
+export interface TrancheWithdrawal {
+	amount: number
+  user: string
+  tx: string
+}
+
+export interface TrancheUser {
+  address: string,
+  deposits: Array<{ amount: number, user: string, tx: string, tranche_id: string }>,
+  withdrawals: Array<{ amount: number, user: string, tx: string, tranche_id: string }>,
+  total_tokens: number
+  withdrawn_tokens: number
+  remaining_tokens: number
 }
