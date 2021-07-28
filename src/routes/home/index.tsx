bc5d0846c1ac271ce8c29af38dd361b4f7bab508
Dexter <dexter.edwards93@gmail.com>
2021-07-28 13:47:08 +0100

    redirect home to tranches

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,22 +1,23 @@
 import "./home.scss";
 
 import React from "react";
-import { NavLink } from "react-router-dom";
-import routerConfig from "../router-config";
+import { /* NavLink, */ Redirect } from "react-router-dom";
+// import routerConfig from "../router-config";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-      <nav className="nav-links">
-        {routerConfig.map((route) => (
-          <NavLink key={route.name} to={route.path}>
-            {route.name}
-          </NavLink>
-        ))}
-      </nav>
-    </div>
-  );
+  return <Redirect to="/tranches" />;
+  // return (
+  //   <div>
+  //     <h1>Home</h1>
+  //     <nav className="nav-links">
+  //       {routerConfig.map((route) => (
+  //         <NavLink key={route.name} to={route.path}>
+  //           {route.name}
+  //         </NavLink>
+  //       ))}
+  //     </nav>
+  //   </div>
+  // );
 };
 
 export default Home;
