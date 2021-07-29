fb66b1038c57872662787771f9894fb5d9ae76e2
Dexter <dexter.edwards93@gmail.com>
2021-07-29 16:09:56 +0100

    don't load tranches info twice

--- src/routes/tranches/index.tsx
+++ src/routes/tranches/index.tsx
@@ -2,25 +2,15 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Tranche } from "./tranche";
 import { Tranches } from "./tranches";
-import type { Tranche as TrancheType } from "../../lib/vega-web3-types";
-import { EthereumChainIds } from "../../lib/vega-web3-utils";
 import { DefaultTemplate } from "../../components/page-templates/default";
 import { useTranslation } from "react-i18next";
-import { useVegaWeb3 } from "../../hooks/use-vega-web3";
+import { useAppState } from "../../contexts/app-state/app-state-context";
 
 const TrancheRouter = () => {
   const { t } = useTranslation();
-  const [tranches, setTranches] = React.useState<TrancheType[]>([]);
-  const vega = useVegaWeb3(EthereumChainIds.Mainnet);
-
-  React.useEffect(() => {
-    async function getTranches() {
-      const res = await vega.getAllTranches();
-      setTranches(res);
-    }
-
-    getTranches();
-  }, [vega]);
+  const {
+    appState: { tranches },
+  } = useAppState();
   return (
     <DefaultTemplate title={t("pageTitleTranches")}>
       <Switch>
