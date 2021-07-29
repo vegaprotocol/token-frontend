a73b83b2e3e4b0f474b1862d621e8411aed415d8
Matthew Russell <mattrussell36@gmail.com>
2021-07-28 20:34:55 -0700

    add hook to connect and new app state

--- src/contexts/app-state/app-state-provider.tsx
+++ src/contexts/app-state/app-state-provider.tsx
@@ -1,4 +1,6 @@
+import detectEthereumProvider from "@metamask/detect-provider";
 import React from "react";
+import { EthereumChainId } from "../../lib/vega-web3-utils";
 import { AppState, AppStateContext, AppStateAction } from "./app-state-context";
 
 interface AppStateProviderProps {
@@ -6,12 +8,19 @@ interface AppStateProviderProps {
 }
 
 const initialAppState: AppState = {
+  hasProvider: false,
   address: null,
   connecting: false,
+  chainId: null,
 };
 
 function appStateReducer(state: AppState, action: AppStateAction) {
   switch (action.type) {
+    case "PROVIDER_DETECTED":
+      return {
+        ...state,
+        hasProvider: true,
+      };
     case "CONNECT":
       return {
         ...state,
@@ -21,6 +30,7 @@ function appStateReducer(state: AppState, action: AppStateAction) {
       return {
         ...state,
         address: action.address,
+        chainId: action.chainId,
         connecting: false,
       };
     case "CONNECT_FAIL":
@@ -29,16 +39,66 @@ function appStateReducer(state: AppState, action: AppStateAction) {
         address: null,
         connecting: false,
       };
+    case "DISCONNECT":
+      return {
+        ...state,
+        address: null,
+      };
+    case "ACCOUNTS_CHANGED": {
+      return {
+        ...state,
+        address: action.address,
+      };
+    }
+    case "CHAIN_CHANGED": {
+      return {
+        ...state,
+        chainId: action.chainId,
+      };
+    }
     default:
       return state;
   }
 }
 
 export function AppStateProvider({ children }: AppStateProviderProps) {
+  const provider = React.useRef<any>();
   const [state, dispatch] = React.useReducer(appStateReducer, initialAppState);
 
+  // Detect provider
+  React.useEffect(() => {
+    detectEthereumProvider().then((res) => {
+      if (res !== null) {
+        provider.current = res;
+        dispatch({ type: "PROVIDER_DETECTED" });
+      }
+    });
+  }, []);
+
+  // Bind listeners for account change
+  React.useEffect(() => {
+    if (state.hasProvider) {
+      provider.current.on("accountsChanged", (accounts: string[]) => {
+        if (accounts.length) {
+          dispatch({ type: "ACCOUNTS_CHANGED", address: accounts[0] });
+        } else {
+          dispatch({ type: "DISCONNECT" });
+        }
+      });
+      provider.current.on("chainChanged", (chainId: EthereumChainId) => {
+        dispatch({ type: "CHAIN_CHANGED", chainId });
+      });
+    }
+  }, [state.hasProvider]);
+
   return (
-    <AppStateContext.Provider value={[state, dispatch]}>
+    <AppStateContext.Provider
+      value={{
+        appState: state,
+        appDispatch: dispatch,
+        provider: provider.current,
+      }}
+    >
       {children}
     </AppStateContext.Provider>
   );
