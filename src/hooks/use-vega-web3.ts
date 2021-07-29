32cb902163d8381cc951ff16b98241e540ca0089
Dexter <dexter.edwards93@gmail.com>
2021-07-29 18:05:12 +0100

    add IVegaWeb3 interface
    adds mocking based on environment variable
    
    
    pass correct address
    
    
    adds mocks
    
    
    adds start mock script

--- src/hooks/use-vega-web3.ts
+++ src/hooks/use-vega-web3.ts
@@ -1,8 +1,12 @@
 import React from "react";
-import VegaWeb3 from "../lib/__mocks__/vega-web3";
+import VegaWeb3Mock from "../lib/__mocks__/vega-web3";
 import { EthereumChainId } from "../lib/vega-web3-utils";
+import VegaWeb3 from "../lib/vega-web3";
+import { IVegaWeb3 } from "../lib/vega-web3-types";
 
 export function useVegaWeb3(chainId: EthereumChainId) {
-  const ref = React.useRef<VegaWeb3>(new VegaWeb3(chainId));
+  const useMocks = ["1", "true"].includes(process.env.REACT_APP_MOCKED!);
+  const Vega = useMocks ? VegaWeb3Mock : VegaWeb3;
+  const ref = React.useRef<IVegaWeb3>(new Vega(chainId));
   return ref.current;
 }
