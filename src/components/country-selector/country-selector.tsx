efe766575825707826808a44907c0819a7695193
Dexter <dexter.edwards93@gmail.com>
2021-07-29 19:46:06 +0100

    adds id to countries

--- src/components/country-selector/country-selector.tsx
+++ src/components/country-selector/country-selector.tsx
@@ -1,30 +1,27 @@
 import React from "react";
-import { countryData } from "./countryData";
+import countryData from "./country-data";
 
 export interface CountrySelectorProps {
-    setIsValidCountry: (isValid: boolean) => void
+  setIsValidCountry: (isValid: boolean) => void;
 }
 
-export const CountrySelector = ({setIsValidCountry}: CountrySelectorProps) => {    
-    const onSelectedCountry = (selectedCountryCode: string) => {
-        const selectedCountry = countryData.filter(country => (country.code === selectedCountryCode))[0]
-        setIsValidCountry(selectedCountry.isValid)
-    }
+export const CountrySelector = ({
+  setIsValidCountry,
+}: CountrySelectorProps) => {
+  const onSelectedCountry = (selectedCountryCode: string) => {
+    const selectedCountry = countryData.filter(
+      (country) => country.code === selectedCountryCode
+    )[0];
+    setIsValidCountry(selectedCountry.isValid);
+  };
 
-    return (
-        <>
-        <select onChange={(value) => onSelectedCountry(value.currentTarget.value)}>
-            {
-                countryData.map(country => (
-                <option
-                    key={country.code}
-                    value={country.code}
-                >
-                    {country.name}
-                </option>
-                ))
-            }
-        </select>
-        </>
-    )
-}
+  return (
+    <select onChange={(value) => onSelectedCountry(value.currentTarget.value)}>
+      {countryData.map((country) => (
+        <option key={country.code} value={country.code}>
+          {country.name}
+        </option>
+      ))}
+    </select>
+  );
+};
