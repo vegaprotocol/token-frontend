6368a8274d5b13271afb0ae747b4996cb050db97
Dexter <dexter.edwards93@gmail.com>
2021-07-30 13:18:12 +0100

    makes transaction in progress pretty

--- src/components/transaction-in-progress/transaction-in-progress.tsx
+++ src/components/transaction-in-progress/transaction-in-progress.tsx
@@ -1,8 +1,12 @@
+import "./transaction-in-progress.scss";
+
 export const TransactionsInProgress = ({ hash }: { hash: string | null }) => {
   return (
-    <div>
+    <div className="transaction-in-progress">
       Transaction in progress.{" "}
-      <a href={`https://etherscan.io/tx/${hash}`}>View on Etherscan</a>
+      <div>
+        <a href={`https://etherscan.io/tx/${hash}`}>View on Etherscan</a>
+      </div>
     </div>
   );
 };
