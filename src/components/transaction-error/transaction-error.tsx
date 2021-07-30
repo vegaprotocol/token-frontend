ca714b639e887f1ea15ee816cf5951ae480b7f36
Dexter <dexter.edwards93@gmail.com>
2021-07-30 15:58:37 +0100

    rebase problems

--- src/components/transaction-error/transaction-error.tsx
+++ src/components/transaction-error/transaction-error.tsx
@@ -1,40 +1,42 @@
-import { useTranslation } from "react-i18next";
-
 import React from "react";
 import { useTranslation } from "react-i18next";
 
 export interface TransactionErrorProps {
-  error: Error | null,
-  hash: string | null,
-  onActionClick?: () => void
+  error: Error | null;
+  hash: string | null;
+  onActionClick?: () => void;
 }
 
-export const TransactionError = ({ error, hash, onActionClick }: TransactionErrorProps) => {
-  const { t } = useTranslation()
+export const TransactionError = ({
+  error,
+  hash,
+  onActionClick,
+}: TransactionErrorProps) => {
+  const { t } = useTranslation();
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      backgroundColor: 'gray',
-      padding: '10px',
-      textAlign: 'center'
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "gray",
+        padding: "10px",
+        textAlign: "center",
       }}
     >
       <h1>{error ? error.message : t("Something went wrong")}</h1>
-      {
-        hash ?
-          <a
-              href={`https://etherscan.io/tx/${hash}`}
-              target="_blank"
-              rel="noreferrer"
-          >
-            {t("View on Etherscan (Opens in a new tab)")}
-          </a>
-          :
-          null
-        }
-    <button onClick={() => onActionClick ? onActionClick() : null}>{t("Try again")}</button>
-  </div>
-  )
+      {hash ? (
+        <a
+          href={`https://etherscan.io/tx/${hash}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {t("View on Etherscan (Opens in a new tab)")}
+        </a>
+      ) : null}
+      <button onClick={() => (onActionClick ? onActionClick() : null)}>
+        {t("Try again")}
+      </button>
+    </div>
+  );
 };
