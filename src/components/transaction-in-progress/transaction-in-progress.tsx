f1ae8b84ae0f15c11c0d7530728c55181f3b932d
Dexter <dexter.edwards93@gmail.com>
2021-07-30 14:50:53 +0100

    some translations

--- src/components/transaction-in-progress/transaction-in-progress.tsx
+++ src/components/transaction-in-progress/transaction-in-progress.tsx
@@ -1,12 +1,14 @@
+import { useTranslation } from "react-i18next";
 import "./transaction-in-progress.scss";
 
 export const TransactionsInProgress = ({ hash }: { hash: string | null }) => {
+  const { t } = useTranslation();
   return (
     <div className="transaction-in-progress">
-      Transaction in progress.{" "}
+      {t("Transaction in progress")}.
       <div>
         <a href={`https://etherscan.io/tx/${hash}`}>
-          View on Etherscan (opens in a new tab)
+          {t("View on Etherscan (opens in a new tab)")}
         </a>
       </div>
     </div>
