14f92a450afbf869c76ffcdd38b843d74350ef9c
Dexter <dexter.edwards93@gmail.com>
2021-07-30 13:23:36 +0100

    chore: makes complete pretty

--- src/routes/claim/claim-form/claim-form.tsx
+++ src/routes/claim/claim-form/claim-form.tsx
@@ -43,7 +43,7 @@ export const ClaimForm = ({
   }
 
   if (state.claimTxState === TxState.Complete) {
-    return <TransactionComplete />;
+    return <TransactionComplete hash={state.claimTxData.hash} />;
   }
 
   return (
