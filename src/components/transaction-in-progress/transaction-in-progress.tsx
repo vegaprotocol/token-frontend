481e259cf0ac457963eee8c4d3ac698bf5e34a1f
Dexter <dexter.edwards93@gmail.com>
2021-07-30 15:12:25 +0100

    remove fulls top

--- src/components/transaction-in-progress/transaction-in-progress.tsx
+++ src/components/transaction-in-progress/transaction-in-progress.tsx
@@ -5,7 +5,7 @@ export const TransactionsInProgress = ({ hash }: { hash: string | null }) => {
   const { t } = useTranslation();
   return (
     <div className="transaction-in-progress">
-      {t("Transaction in progress")}.
+      {t("Transaction in progress")}
       <div>
         <a href={`https://etherscan.io/tx/${hash}`}>
           {t("View on Etherscan (opens in a new tab)")}
