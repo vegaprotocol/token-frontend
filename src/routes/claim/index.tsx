741afb298b0e8612d6e25d6f2bad01338268ea11
Dexter <dexter.edwards93@gmail.com>
2021-07-31 15:33:23 +0100

    make claim router as stupid as possible

--- src/routes/claim/index.tsx
+++ src/routes/claim/index.tsx
@@ -12,9 +12,6 @@ import { claimReducer, initialClaimState } from "./claim-form/claim-reducer";
 import { ConnectedClaim } from "./connected";
 import { ClaimRestricted } from "./claim-restricted";
 import { isRestricted } from "./lib/is-restricted";
-import detectEthereumProvider from "@metamask/detect-provider";
-import Web3 from "web3";
-import VegaClaim from "../../lib/vega-claim";
 
 const ClaimRouter = () => {
   const { t } = useTranslation();
@@ -39,65 +36,6 @@ const ClaimRouter = () => {
     };
     run();
   }, [dispatch, params, vega]);
-  const commitClaim = React.useCallback(async () => {
-    dispatch({ type: "CLAIM_TX_REQUESTED" });
-    const provider = (await detectEthereumProvider()) as any;
-    const web3 = new Web3(provider);
-    const claim = new VegaClaim(
-      web3,
-      "0xAf5dC1772714b2F4fae3b65eb83100f1Ea677b21"
-    );
-    claim
-      .commit(state.code!, appState.address!)
-      .once("transactionHash", (hash: string) => {
-        dispatch({ type: "CLAIM_TX_SUBMITTED", txHash: hash });
-      })
-      .once("receipt", (receipt: any) => {
-        dispatch({ type: "CLAIM_TX_COMPLETE", receipt });
-      })
-      .once("error", (err: Error) => {
-        console.log(err);
-        dispatch({ type: "CLAIM_TX_ERROR", error: err });
-      });
-  }, [appState.address, state.code]);
-  const claim = React.useCallback(async () => {
-    dispatch({ type: "CLAIM_TX_REQUESTED" });
-    const provider = (await detectEthereumProvider()) as any;
-    const web3 = new Web3(provider);
-    const claim = new VegaClaim(
-      web3,
-      "0xAf5dC1772714b2F4fae3b65eb83100f1Ea677b21"
-    );
-    claim
-      .claim({
-        claimCode: state.code!,
-        denomination: state.denomination!,
-        trancheId: state.trancheId!,
-        expiry: state.expiry!,
-        nonce: state.nonce!,
-        country: "GB",
-        targeted: !!state.target,
-        account: appState.address!,
-      })
-      .once("transactionHash", (hash: string) => {
-        dispatch({ type: "CLAIM_TX_SUBMITTED", txHash: hash });
-      })
-      .once("receipt", (receipt: any) => {
-        dispatch({ type: "CLAIM_TX_COMPLETE", receipt });
-      })
-      .once("error", (err: Error) => {
-        console.log(err);
-        dispatch({ type: "CLAIM_TX_ERROR", error: err });
-      });
-  }, [
-    appState.address,
-    state.code,
-    state.denomination,
-    state.expiry,
-    state.nonce,
-    state.target,
-    state.trancheId,
-  ]);
 
   let pageContent;
 
@@ -106,14 +44,7 @@ const ClaimRouter = () => {
   } else if (state.error) {
     pageContent = <ClaimError />;
   } else if (appState.address) {
-    pageContent = (
-      <ConnectedClaim
-        state={state}
-        commitClaim={commitClaim}
-        claim={claim}
-        dispatch={dispatch}
-      />
-    );
+    pageContent = <ConnectedClaim state={state} dispatch={dispatch} />;
   } else {
     pageContent = (
       <section>
