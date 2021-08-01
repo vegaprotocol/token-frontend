0d77f2fda5ae87b71b0a329c35503a112ca909f8
Dexter <dexter.edwards93@gmail.com>
2021-08-01 14:16:51 +0100

    build vega vesting to be it's own class

--- src/lib/web3-utils.ts
+++ src/lib/web3-utils.ts
@@ -21,3 +21,16 @@ export const EthereumChainIds: Record<EthereumChainName, EthereumChainId> = {
   Goerli: "0x5",
   Kovan: "0x2a",
 };
+
+export const Addresses = {
+  [EthereumChainIds.Mainnet]: {
+    vestingAddress: "0x23d1bFE8fA50a167816fBD79D7932577c06011f4",
+    vegaTokenAddress: "0xcB84d72e61e383767C4DFEb2d8ff7f4FB89abc6e",
+    claimAddress: "",
+  },
+  [EthereumChainIds.Ropsten]: {
+    vestingAddress: "0x08C06ECDCf9b8f45e3cF1ec29B82eFd0171341D9",
+    vegaTokenAddress: "0x16480156222D4525f02F0F2BdF8A45A23bd26431",
+    claimAddress: "0xAf5dC1772714b2F4fae3b65eb83100f1Ea677b21",
+  },
+};
