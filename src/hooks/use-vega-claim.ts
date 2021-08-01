fb55dfa48cb071ed00f6191f2bae80c6f179217f
Dexter <dexter.edwards93@gmail.com>
2021-08-01 15:39:42 +0100

    use addresses based on what chainId is set

--- src/hooks/use-vega-claim.ts
+++ src/hooks/use-vega-claim.ts
@@ -2,12 +2,16 @@ import React from "react";
 import Web3 from "web3";
 import { useAppState } from "../contexts/app-state/app-state-context";
 import VegaClaim from "../lib/vega-web3/vega-claim";
+import { Addresses } from "../lib/web3-utils";
 
 export const useVegaClaim = () => {
-  const { provider } = useAppState();
+  const {
+    provider,
+    appState: { appChainId },
+  } = useAppState();
   const claim = React.useMemo(() => {
     const web3 = new Web3(provider);
-    return new VegaClaim(web3, "0xAf5dC1772714b2F4fae3b65eb83100f1Ea677b21");
-  }, [provider]);
+    return new VegaClaim(web3, Addresses[appChainId].claimAddress);
+  }, [appChainId, provider]);
   return claim;
 };
--- src/hooks/use-vega-vesting.ts
+++ src/hooks/use-vega-vesting.ts
@@ -2,15 +2,19 @@ import React from "react";
 import VegaVesting from "../lib/vega-web3/vega-vesting";
 import Web3 from "web3";
 import { useAppState } from "../contexts/app-state/app-state-context";
+import { Addresses } from "../lib/web3-utils";
 
 export function useVegaVesting() {
-  const { provider } = useAppState();
+  const {
+    provider,
+    appState: { appChainId },
+  } = useAppState();
   // TODO make mocks work again
   // const useMocks = ["1", "true"].includes(process.env.REACT_APP_MOCKED!);
   const vesting = React.useMemo(() => {
     const web3 = new Web3(provider);
-    return new VegaVesting(web3, "0x08C06ECDCf9b8f45e3cF1ec29B82eFd0171341D9");
-  }, [provider]);
+    return new VegaVesting(web3, Addresses[appChainId].vestingAddress);
+  }, [appChainId, provider]);
   const ref = React.useRef(vesting);
   return ref.current;
 }
