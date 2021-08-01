424880fffa3764a19b44a990b99a646b75919378
Dexter <dexter.edwards93@gmail.com>
2021-08-01 11:32:23 +0100

    seperate locked banner, adds translations for expired

--- src/routes/claim/code-used.tsx
+++ src/routes/claim/code-used.tsx
@@ -1,4 +1,5 @@
 import { Trans } from "react-i18next";
+import { LockedBanner } from "./locked-banner";
 
 export const CodeUsed = ({ address }: { address: string | null }) => {
   return (
@@ -11,21 +12,7 @@ export const CodeUsed = ({ address }: { address: string | null }) => {
         />
       </p>
       <p>Looks like that code has already been used.</p>
-      <div style={{ padding: 20, border: "1px solid white" }}>
-        <p>
-          Keep track of locked tokens in your wallet with the VEGA (VESTING)
-          token.
-        </p>
-        <p>
-          Add the Vega vesting token to your wallet to track how much you Vega
-          you have in the vesting contract.
-        </p>
-        <p>
-          {/* TODO get the address for the locked vega contract */}
-          The token address is 0xufb8y3bv8wur4uhr. Hit the add token button in
-          your ERC20 wallet and enter this address.
-        </p>
-      </div>
+      <LockedBanner />
     </div>
   );
 };
