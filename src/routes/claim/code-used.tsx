3367937a3545da651f18b8f771100ae74f09cda3
Dexter <dexter.edwards93@gmail.com>
2021-08-01 11:06:59 +0100

    build code used and code expired pages

---
+++ src/routes/claim/code-used.tsx
@@ -0,0 +1,31 @@
+import { Trans } from "react-i18next";
+
+export const CodeUsed = ({ address }: { address: string | null }) => {
+  return (
+    <div>
+      <p>
+        <Trans
+          i18nKey="Connected to Ethereum key {address}"
+          values={{ address }}
+          components={{ bold: <strong /> }}
+        />
+      </p>
+      <p>Looks like that code has already been used.</p>
+      <div style={{ padding: 20, border: "1px solid white" }}>
+        <p>
+          Keep track of locked tokens in your wallet with the VEGA (VESTING)
+          token.
+        </p>
+        <p>
+          Add the Vega vesting token to your wallet to track how much you Vega
+          you have in the vesting contract.
+        </p>
+        <p>
+          {/* TODO get the address for the locked vega contract */}
+          The token address is 0xufb8y3bv8wur4uhr. Hit the add token button in
+          your ERC20 wallet and enter this address.
+        </p>
+      </div>
+    </div>
+  );
+};
