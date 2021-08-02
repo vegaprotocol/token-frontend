0f1912c9f5b24c05fa06b8aaee2796cf399000e6
Matthew Russell <mattrussell36@gmail.com>
2021-08-02 12:38:43 -0700

    add component for redeem info and put derive tranche state inside

---
+++ src/routes/claim/redeem-info.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { format } from "date-fns";
+import { Tranche } from "../../lib/vega-web3/vega-web3-types";
+
+interface RedeemInfoProps {
+  tranche: Tranche;
+}
+
+export const RedeemInfo = ({ tranche }: RedeemInfoProps) => {
+  const showRedeem = ["1", "true"].includes(process.env.REACT_APP_REDEEM_LIVE!);
+  const unlockDate = format(
+    new Date(tranche.tranche_end).getTime(),
+    "MMM d, yyyy"
+  );
+  const trancheEndDate = format(
+    new Date(tranche.tranche_start).getTime(),
+    "MMM d, yyyy"
+  );
+  const fullyRedeemable =
+    new Date().getTime() > new Date(tranche.tranche_end).getTime();
+  const partiallyRedeemable =
+    !fullyRedeemable &&
+    new Date().getTime() > new Date(tranche.tranche_start).getTime();
+  const noneRedeemable = !fullyRedeemable && !partiallyRedeemable;
+
+  return (
+    <>
+      {noneRedeemable && (
+        <p>
+          {t("tranche description", {
+            unlockDate,
+            trancheEndDate,
+          })}{" "}
+          {showRedeem && t("none redeemable")}
+        </p>
+      )}
+      {partiallyRedeemable && (
+        <p>
+          {t("tranche description", {
+            unlockDate,
+            trancheEndDate,
+          })}{" "}
+          {showRedeem && t("partially redeemable")}
+        </p>
+      )}
+      {fullyRedeemable && (
+        <p>
+          {t("Tokens in this tranche are fully unlocked.")}
+          {showRedeem && t("fully redeemable")}
+        </p>
+      )}
+    </>
+  );
+};
