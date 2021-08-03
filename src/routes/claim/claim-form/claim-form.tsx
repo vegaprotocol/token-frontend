c86d13d07c3b541f0b3acb94d1fd292dea5602dc
Dexter <dexter.edwards93@gmail.com>
2021-08-03 09:27:36 +0100

    move country selector back

--- src/routes/claim/claim-form/claim-form.tsx
+++ src/routes/claim/claim-form/claim-form.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { CountrySelector } from "../../../components/country-selector";
 import { TransactionCallout } from "../../../components/transaction-callout";
 import { useAppState } from "../../../contexts/app-state/app-state-context";
-import { ClaimAction } from "../claim-reducer";
-import { useValidateCountry } from "../hooks";
 
 import {
   TransactionAction,
@@ -18,29 +15,24 @@ export interface ICountry {
 }
 
 export const ClaimForm = ({
-  dispatch,
   txState,
   txDispatch,
   onSubmit,
   completed,
+  isValid,
+  loading,
 }: {
-  dispatch: React.Dispatch<ClaimAction>;
   txState: TransactionState;
   txDispatch: React.Dispatch<TransactionAction>;
   onSubmit: () => void;
   completed: boolean;
+  isValid: boolean;
+  loading: boolean;
 }) => {
   const { t } = useTranslation();
   const {
     appState: { chainId },
   } = useAppState();
-  const { country, checkCountry, isValid, loading } = useValidateCountry();
-
-  React.useEffect(() => {
-    if (country) {
-      dispatch({ type: "SET_COUNTRY", countryCode: country.code });
-    }
-  }, [country, dispatch]);
 
   if (txState.txState !== TxState.Default || completed) {
     return (
@@ -60,14 +52,6 @@ export const ClaimForm = ({
         onSubmit();
       }}
     >
-      <CountrySelector setCountry={checkCountry} />
-      {!isValid && country?.code && (
-        <div style={{ color: "#ED1515", marginBottom: 20 }}>
-          {t(
-            "Sorry. It is not possible to claim tokens in your country or region."
-          )}
-        </div>
-      )}
       <button disabled={!isValid || loading} type="submit">
         {loading ? t("Loading") : t("Continue")}
       </button>
