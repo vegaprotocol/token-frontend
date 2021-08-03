8cc0fab48d72b69d338124c74ad2d091d2c927a1
Dexter <dexter.edwards93@gmail.com>
2021-08-03 14:05:03 +0100

    use mocks based on env vars

--- src/hooks/use-vega-claim.ts
+++ src/hooks/use-vega-claim.ts
@@ -2,15 +2,20 @@ import React from "react";
 import Web3 from "web3";
 import { useAppState } from "../contexts/app-state/app-state-context";
 import VegaClaim from "../lib/vega-web3/vega-claim";
+import { IVegaClaim } from "../lib/web3-utils";
+import MockedVegaClaim from "../lib/__mocks__/vega-claim";
 
 export const useVegaClaim = () => {
   const {
     provider,
     appState: { contractAddresses },
   } = useAppState();
-  const claim = React.useMemo(() => {
+  const claim = React.useMemo<IVegaClaim>(() => {
+    const useMocks = ["1", "true"].includes(process.env.REACT_APP_MOCKED!);
     const web3 = new Web3(provider);
-    return new VegaClaim(web3, contractAddresses.claimAddress);
-  }, [provider, contractAddresses.claimAddress]);
+    return useMocks
+      ? new MockedVegaClaim()
+      : new VegaClaim(web3, contractAddresses.claimAddress);
+  }, [contractAddresses.claimAddress, provider]);
   return claim;
 };
