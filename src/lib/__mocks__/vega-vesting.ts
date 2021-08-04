24d3a832576401bdf201ac1b8dd20497470b3ea4
Dexter <dexter.edwards93@gmail.com>
2021-08-04 10:33:57 +0100

    use fetch requests to populate test data

--- src/lib/__mocks__/vega-vesting.ts
+++ src/lib/__mocks__/vega-vesting.ts
@@ -2,9 +2,17 @@ import { Tranche } from "../vega-web3/vega-web3-types";
 import { IVegaVesting } from "../web3-utils";
 import trancheData from "./tranche-data.json";
 
+const BASE_URL = "mocks/vesting";
+
 class MockedVesting implements IVegaVesting {
-  getUserBalanceAllTranches(account: string): Promise<string> {
-    throw new Error("Method not implemented.");
+  private async performFetch(url: string) {
+    const res = await fetch(`${BASE_URL}/${url}`);
+    return await res.json();
+  }
+
+  async getUserBalanceAllTranches(account: string): Promise<string> {
+    const balance = await this.performFetch(`balance`);
+    return Promise.resolve(balance);
   }
   getAllTranches(): Promise<Tranche[]> {
     // TODO populate with events
