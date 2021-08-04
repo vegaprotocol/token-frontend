e523bd59aeff9e5359fd8677c8c41a731565eff5
Dexter <dexter.edwards93@gmail.com>
2021-08-04 09:56:39 +0100

    Simplify 404

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Loading } from "../components/loading";
 import { useAppState } from "../contexts/app-state/app-state-context";
-import NotFound from "./not-found";
 import routerConfig from "./router-config";
 
 export const AppRouter = () => {
@@ -17,7 +16,6 @@ export const AppRouter = () => {
             <Route key={name} path={path} exact={exact} component={Component} />
           )
         )}
-        <Route component={NotFound} />
       </Switch>
     </React.Suspense>
   ) : (
