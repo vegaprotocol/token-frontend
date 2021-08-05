1d3a76f8baa2bd0d6693f29d0b0f91737e536219
Pablo Griscti <2323dragon@gmail.com>
2021-08-05 12:01:02 +0100

    moves continue button to be its own component

---
+++ src/components/continue-button/continue-button.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./continue-button.scss";
+
+import { useTranslation } from "react-i18next";
+import { Colors } from "../../colors";
+
+export interface ContinueButtonProps {
+  isValid: boolean;
+  loading: boolean;
+  onSubmit: () => void;
+  continueText: string;
+  errorText: string;
+}
+
+export const ContinueButton = ({
+  isValid,
+  loading,
+  onSubmit,
+  continueText,
+  errorText,
+}: ContinueButtonProps) => {
+  const [showError, setShowError] = React.useState(false);
+  const { t } = useTranslation();
+
+  const disabled = !isValid || loading;
+  const onContinue = () => {
+    setShowError(disabled);
+    if (!disabled) {
+      onSubmit();
+    }
+  };
+  return (
+    <div className="continue-button__container">
+      <button
+        onClick={onContinue}
+        className={disabled ? "continue-button__disabled" : ""}
+      >
+        {loading ? t("Loading") : continueText}
+      </button>
+      {showError && !isValid ? (
+        <p style={{ color: Colors.RED }}>{errorText}</p>
+      ) : null}
+    </div>
+  );
+};
