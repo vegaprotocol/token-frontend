3d46e4180858e976c6f0b444627516736abf4552
Dexter <dexter.edwards93@gmail.com>
2021-08-06 14:16:44 +0100

    Redirect from no-provider if the provider has subsequently been found

--- src/routes/no-provider/index.tsx
+++ src/routes/no-provider/index.tsx
@@ -1,8 +1,19 @@
 import { useTranslation } from "react-i18next";
+import { Redirect } from "react-router-dom";
 import { DefaultTemplate } from "../../components/page-templates/default";
+import {
+  ProviderStatus,
+  useAppState,
+} from "../../contexts/app-state/app-state-context";
 
 const NoProvider = () => {
   const { t } = useTranslation();
+  const {
+    appState: { providerStatus },
+  } = useAppState();
+  if (providerStatus === ProviderStatus.Ready) {
+    return <Redirect to="/" />;
+  }
   return (
     <DefaultTemplate title={t("Cannot connect")}>
       <p>{t("invalidWeb3Browser")}</p>
