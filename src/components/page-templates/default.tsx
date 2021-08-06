3bf55b04890bd9c0e8cbcb008e408f1bbcb17224
Dexter <dexter.edwards93@gmail.com>
2021-08-06 14:25:44 +0100

    better wrong chain state

--- src/components/page-templates/default.tsx
+++ src/components/page-templates/default.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useAppState } from "../../contexts/app-state/app-state-context";
 import { Heading } from "../heading";
 import { Notice } from "../notice";
+import { WrongChain } from "../wrong-chain";
 
 export interface DefaultTemplateProps {
   children: React.ReactNode;
@@ -8,10 +10,20 @@ export interface DefaultTemplateProps {
 }
 
 export function DefaultTemplate({ children, title }: DefaultTemplateProps) {
+  const { appState } = useAppState();
+  let splash = null;
+  if (appState.chainId !== appState.appChainId) {
+    splash = (
+      <WrongChain
+        currentChainId={appState.chainId!}
+        desiredChainId={appState.appChainId}
+      />
+    );
+  }
   return (
     <div className="app-wrapper">
       <Heading title={title} />
-      <main>{children}</main>
+      <main>{splash ? splash : children}</main>
       <footer>
         <Notice />
       </footer>
--- src/components/web3-container/web3-container.tsx
+++ src/components/web3-container/web3-container.tsx
@@ -4,19 +4,11 @@ import {
   ProviderStatus,
   useAppState,
 } from "../../contexts/app-state/app-state-context";
-import { WrongChain } from "../wrong-chain";
 
 export const Web3Container = ({ children }: { children?: React.ReactNode }) => {
   const { appState } = useAppState();
   if (appState.providerStatus === ProviderStatus.None) {
     return <Redirect to="/no-provider" />;
-  } else if (appState.chainId !== appState.appChainId) {
-    return (
-      <WrongChain
-        currentChainId={appState.chainId!}
-        desiredChainId={appState.appChainId}
-      />
-    );
   }
 
   return <>{children}</>;
