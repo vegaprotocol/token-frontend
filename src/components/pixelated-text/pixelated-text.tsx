795d39831ba43c996b5c261ba3588dd3bf7177a0
Matthew Russell <mattrussell36@gmail.com>
2021-08-06 22:16:35 -0700

    memoize pixelated text so it doesnt change on render

--- src/components/pixelated-text/pixelated-text.tsx
+++ src/components/pixelated-text/pixelated-text.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export const PixelatedText = ({
   text,
   threshold = 0.6,
@@ -5,16 +7,18 @@ export const PixelatedText = ({
   text: string;
   threshold?: number;
 }) => {
-  return (
-    <>
-      {text.split("").map((char, i) => (
-        <span
-          key={i}
-          className={Math.random() > threshold ? "text-pixelated" : ""}
-        >
-          {char}
-        </span>
-      ))}
-    </>
-  );
+  // useMemo so it doesn't re-randomize the text on every render
+  const chars = React.useMemo(() => {
+    console.log("update spans");
+    return text.split("").map((char, i) => (
+      <span
+        key={i}
+        className={Math.random() > threshold ? "text-pixelated" : ""}
+      >
+        {char}
+      </span>
+    ));
+  }, [text, threshold]);
+
+  return <>{chars}</>;
 };
