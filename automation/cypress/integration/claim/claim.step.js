a9326d8eedba9453cd1c1794b4a31dbbfcd9c4ad
Ditmir <ditmir@ditmirs-mbp-2.home>
2021-08-12 09:03:38 +0100

    update cypress dashboard config

--- automation/cypress/integration/claim/claim.step.js
+++ automation/cypress/integration/claim/claim.step.js
@@ -1,3 +1,11 @@
 import { When, Then, Given } from 'cypress-cucumber-preprocessor/steps';
 
+Given('I am on the tranches page',()=>{
+  cy.visit('/');
+  cy.url().should('include','tranches')
+})
+
+Then('I can see the header title is {string}',(headerTitle)=>{
+  cy.get('.heading__title').should('have.text',headerTitle)
+})
 
