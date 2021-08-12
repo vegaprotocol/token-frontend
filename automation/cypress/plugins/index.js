8fcb0ac5296293e05ebda57c15385803602a507d
Ditmir <ditmir@ditmirs-mbp-2.home>
2021-08-12 09:03:38 +0100

    update cypress dashboard config

--- automation/cypress/plugins/index.js
+++ automation/cypress/plugins/index.js
@@ -8,16 +8,9 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 const { cypressConfigResolver } = require('../config/cypress-config-resolver');
-// const pluginExecuteCommand = require('./plugin-execute-command');
 const cucumber = require('cypress-cucumber-preprocessor').default;
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  on('file:preprocessor', cucumber());
-
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('file:preprocessor', cucumber());s
   return cypressConfigResolver();
 };
