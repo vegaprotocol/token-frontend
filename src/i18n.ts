0d4aec3008db5b655a1045907c8db153ba74d55e
Dexter <dexter.edwards93@gmail.com>
2021-08-16 17:45:24 +0100

    chore: double space

--- src/i18n.ts
+++ src/i18n.ts
@@ -114,7 +114,7 @@ const en = {
     "This service is not available in your country":
       "This service is not available in your country/region",
     "Wrong network": "Looks like you are on {{chain}}",
-    "Desired network": "This app is only configured for  {{chain}}",
+    "Desired network": "This app is only configured for {{chain}}",
     "This code ({code}) has expired and cannot be used to claim tokens":
       "This code ({{code}}) has expired and cannot be used to claim tokens.",
     "Looks like that code has already been used.":
