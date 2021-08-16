721340afe6a34f7596a3783e342f1cfb18327d79
Dexter <dexter.edwards93@gmail.com>
2021-08-16 15:54:46 +0100

    feat: add typeahead

--- src/components/country-selector/country-selector.tsx
+++ src/components/country-selector/country-selector.tsx
@@ -1,24 +1,57 @@
 import countryData from "./country-data";
+import { ItemPredicate, Suggest } from "@blueprintjs/select";
+import { MenuItem } from "@blueprintjs/core";
+import { ICountry } from "../../routes/claim/claim-form";
+
+const CountrySuggest = Suggest.ofType<ICountry>();
+
+export const filterCountry: ItemPredicate<ICountry> = (
+  query,
+  country,
+  _index,
+  exactMatch
+) => {
+  const normalizedTitle = country.name.toLowerCase();
+  const normalizedQuery = query.toLowerCase();
+
+  if (exactMatch) {
+    return normalizedTitle === normalizedQuery;
+  } else if (query.length === 2) {
+    return normalizedQuery === country.code.toLowerCase();
+  } else {
+    return normalizedTitle.indexOf(normalizedQuery) >= 0;
+  }
+};
 
 export interface CountrySelectorProps {
   onSelectCountry: (countryCode: string) => void;
+  code: string | null;
 }
 
-export const CountrySelector = ({ onSelectCountry }: CountrySelectorProps) => {
-  const name = "country-selector";
-
+export const CountrySelector = ({
+  onSelectCountry,
+  code,
+}: CountrySelectorProps) => {
   return (
-    <select
-      name={name}
-      id={name}
-      className="country-selector"
-      onChange={(value) => onSelectCountry(value.currentTarget.value)}
-    >
-      {countryData.map((country) => (
-        <option key={country.code} value={country.code}>
-          {country.name}
-        </option>
-      ))}
-    </select>
+    <CountrySuggest
+      selectedItem={countryData.find((c) => c.code === code) || countryData[0]}
+      items={countryData}
+      itemRenderer={(item, { handleClick, modifiers }) => (
+        <MenuItem
+          key={item.code}
+          text={item.name}
+          active={modifiers.active}
+          disabled={modifiers.disabled}
+          onClick={handleClick}
+        />
+      )}
+      onItemSelect={(item) => {
+        onSelectCountry(item.code);
+      }}
+      inputValueRenderer={(item) => item.name}
+      popoverProps={{ minimal: true }}
+      noResults={<MenuItem disabled={true} text="No results." />}
+      itemPredicate={filterCountry}
+    />
   );
 };
