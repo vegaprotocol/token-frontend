f3d23544f95ea0387d8ddf3381bb12ad24eed236
Dexter <dexter.edwards93@gmail.com>
2021-08-16 18:42:30 +0100

    Revert "fix bad mocks"
    This reverts commit 3d0ffda97d705dbb85fee06ef4ca5966de5aea78.

--- craco.config.js
+++ craco.config.js
@@ -3,8 +3,7 @@ const webpack = require("webpack");
 module.exports = function () {
   const isMock = ["1", "true"].includes(process.env.REACT_APP_MOCKED);
   const detectProviderPath = isMock ? "../../__mocks__/@metamask" : "@metamask";
-  const vegaVesting = isMock ? "vega-web3/__mocks__" : "vega-vesting";
-  const vegaClaim = isMock ? "vega-web3/__mocks__" : "vega-claim";
+  const vegaWeb3Path = isMock ? "vega-web3/__mocks__" : "vega-web3";
 
   return {
     webpack: {
@@ -19,20 +18,11 @@ module.exports = function () {
           }
         ),
         new webpack.NormalModuleReplacementPlugin(
-          /(.*)VEGA_VESTING(\.*)/,
+          /(.*)VEGA_WEB3(\.*)/,
           function (resource) {
             resource.request = resource.request.replace(
-              /VEGA_VESTING/,
-              `${vegaVesting}`
-            );
-          }
-        ),
-        new webpack.NormalModuleReplacementPlugin(
-          /(.*)VEGA_CLAIM(\.*)/,
-          function (resource) {
-            resource.request = resource.request.replace(
-              /VEGA_CLAIM/,
-              `${vegaClaim}`
+              /VEGA_WEB3/,
+              `${vegaWeb3Path}`
             );
           }
         ),
