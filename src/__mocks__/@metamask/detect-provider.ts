e91b7b764b74017fed65008cca92eb95ea7eeaed
Dexter <dexter.edwards93@gmail.com>
2021-08-17 09:40:34 +0100

    mocks out provider provided information

--- src/__mocks__/@metamask/detect-provider.ts
+++ src/__mocks__/@metamask/detect-provider.ts
@@ -1,19 +1,34 @@
-import { EthereumChainIds } from "../../lib/web3-utils";
-
-const address = "0x" + "0".repeat(30);
+const BASE_URL = "../mocks/detect-provider";
 
 export default function detectEthereumProvider(): Promise<any> {
   class MockedProvider {
+    private async performFetch<T>(url: string, data?: any): Promise<T> {
+      if (data) {
+        const res = await fetch(`${BASE_URL}/${url}`, {
+          method: "POST",
+          body: JSON.stringify(data),
+        });
+        return res.json() as unknown as T;
+      } else {
+        const res = await fetch(`${BASE_URL}/${url}`);
+        return res.json() as unknown as T;
+      }
+    }
+
     on(event: string, callback: () => void) {
       console.log(`Adding listener for event ${event}`);
     }
 
-    request(obj: any) {
+    async request(obj: any) {
       switch (obj.method) {
         case "eth_requestAccounts":
-          return Promise.resolve([address]);
+          const { address } = await this.performFetch<{ address: string }>(
+            "address"
+          );
+          return address;
         case "eth_chainId":
-          return Promise.resolve(EthereumChainIds.Ropsten);
+          const { chain } = await this.performFetch<{ chain: string }>("chain");
+          return chain;
         default:
           throw new Error(`No mock for provider method ${obj.method}`);
       }
