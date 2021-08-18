16ae737ffb18ed047e4dca2154c13da79859f3fb
Dexter <dexter.edwards93@gmail.com>
2021-08-18 09:59:10 +0100

    update tranche helpers

--- src/lib/vega-web3/__mocks__/vega-vesting.ts
+++ src/lib/vega-web3/__mocks__/vega-vesting.ts
@@ -2,10 +2,17 @@ import BigNumber from "bignumber.js";
 import { Tranche } from "../vega-web3-types";
 import { IVegaVesting } from "../../web3-utils";
 import { getTranchesFromHistory } from "../tranche-helpers";
+import Web3 from "web3";
 
 const BASE_URL = "../mocks/vesting";
 
 class MockedVesting implements IVegaVesting {
+  private decimals: number;
+
+  constructor(web3: Web3, vestingAddress: string, decimals: number) {
+    this.decimals = decimals;
+  }
+
   private async performFetch(url: string) {
     const res = await fetch(`${BASE_URL}/${url}`);
     return await res.json();
@@ -18,7 +25,7 @@ class MockedVesting implements IVegaVesting {
 
   async getAllTranches(): Promise<Tranche[]> {
     const events = await this.performFetch("events");
-    return getTranchesFromHistory(events);
+    return getTranchesFromHistory(events, this.decimals);
   }
 }
 
