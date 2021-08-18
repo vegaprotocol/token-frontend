fdc6b2caf7527bdf941a170dac75f0e65766b3ff
Matthew Russell <mattrussell36@gmail.com>
2021-08-18 13:17:26 -0700

    move default template styles to pdefault.scss

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import "./i18n";
-import "./app.scss";
 import { AppRouter } from "./routes";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AppStateProvider } from "./contexts/app-state/app-state-provider";
