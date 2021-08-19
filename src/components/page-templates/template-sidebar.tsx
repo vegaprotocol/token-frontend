b0e54e15c1ac16e58a33bbb5d44b23349414b952
Matthew Russell <mattrussell36@gmail.com>
2021-08-19 00:35:29 -0700

    add a drawer for small screens

--- src/components/page-templates/template-sidebar.tsx
+++ src/components/page-templates/template-sidebar.tsx
@@ -2,6 +2,7 @@ import "./template-sidebar.scss";
 import React from "react";
 import { Heading } from "../heading";
 import { Notice } from "../notice";
+import { Drawer } from "@blueprintjs/core";
 
 export interface TemplateSidebarProps {
   title: string;
@@ -14,17 +15,44 @@ export function TemplateSidebar({
   children,
   sidebar,
 }: TemplateSidebarProps) {
+  const isDesktop = window.innerWidth > 960;
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
   return (
     <div className="template-sidebar">
       <Heading title={title} />
       <main>{children}</main>
-      <aside>
-        {/* TODO: whats the best way to do the below, appeasing typescript
+      {isDesktop ? (
+        <aside>
+          {/* TODO: whats the best way to do the below, appeasing typescript
          // @ts-ignore */}
-        {sidebar.map((Component, i) => (
-          <section key={i}>{Component}</section>
-        ))}
-      </aside>
+          {sidebar.map((Component, i) => (
+            <section key={i}>{Component}</section>
+          ))}
+        </aside>
+      ) : (
+        <>
+          <button
+            type="button"
+            onClick={() => setDrawerOpen(true)}
+            style={{ position: "fixed", top: 20, right: 20 }}
+          >
+            View keys
+          </button>
+          <Drawer
+            isOpen={drawerOpen}
+            onClose={() => setDrawerOpen(false)}
+            size="80%"
+          >
+            <div>
+              {/* @ts-ignore */}
+              {sidebar.map((Component, i) => (
+                <section key={i}>{Component}</section>
+              ))}
+            </div>
+          </Drawer>
+        </>
+      )}
       <footer>
         <Notice />
       </footer>
