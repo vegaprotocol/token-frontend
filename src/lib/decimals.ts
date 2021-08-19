cad5f454aab93a6799f4220f029d0406c95ffd3c
Edd <edd@vega.xyz>
2021-08-19 11:09:47 +0100

    Switch from toFixed to decimalPlaces in addDecimal

--- src/lib/decimals.ts
+++ src/lib/decimals.ts
@@ -1,7 +1,7 @@
-import { BigNumber } from "bignumber.js";
+import { BigNumber } from '../lib/bignumber'
 
 export function addDecimal(value: BigNumber, decimals: number): string {
-  return value.dividedBy(Math.pow(10, decimals)).toFixed(decimals);
+  return value.dividedBy(Math.pow(10, decimals)).decimalPlaces(decimals).toString();
 }
 export function removeDecimal(value: BigNumber, decimals: number): string {
   return value.times(Math.pow(10, decimals)).toFixed(0);
