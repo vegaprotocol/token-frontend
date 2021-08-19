2a348162d435d013cd313ad5f041e32e25514414
Dexter <dexter.edwards93@gmail.com>
2021-08-19 10:15:21 +0100

    adds loading state while data is coming from the contract

--- src/routes/redemption/index.tsx
+++ src/routes/redemption/index.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import { RouteChildProps } from "..";
 import { Connect } from "../../components/connect";
 import { DefaultTemplate } from "../../components/page-templates/default";
+import { SplashLoader } from "../../components/splash-loader";
 import { TrancheContainer } from "../../components/tranche-container";
 import { Web3Container } from "../../components/web3-container";
 import { useAppState } from "../../contexts/app-state/app-state-context";
@@ -30,9 +31,6 @@ const RedemptionRouter = ({ name }: RouteChildProps) => {
       loading: true,
     });
     try {
-      const unlockedBalance = 0.0005;
-      const lockedBalance = 0.0005;
-      const stakedBalance = 0.0005;
       const userTranches =
         tranches?.filter((t) =>
           t.users.some(({ address: a }) => a === address)
@@ -49,12 +47,14 @@ const RedemptionRouter = ({ name }: RouteChildProps) => {
     } finally {
       dispatch({
         type: "SET_LOADING",
-        loading: true,
+        loading: false,
       });
     }
   }, [address, tranches]);
   let pageContent = null;
-  if (state.error) {
+  if (state.loading) {
+    pageContent = <SplashLoader />;
+  } else if (state.error) {
     throw new Error("Implement this dexter you dumbass");
   } else if (!appState.address) {
     pageContent = <Connect />;
