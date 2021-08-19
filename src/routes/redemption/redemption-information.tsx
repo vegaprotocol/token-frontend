24956d647d865aea6742824359fa38b4d3f27a1e
Dexter <dexter.edwards93@gmail.com>
2021-08-19 09:40:57 +0100

    adds empty state if the user has not tokens in empty tranche

--- src/routes/redemption/redemption-information.tsx
+++ src/routes/redemption/redemption-information.tsx
@@ -2,18 +2,30 @@ import { useAppState } from "../../contexts/app-state/app-state-context";
 
 export const RedemptionInformation = () => {
   const {
-    appState: { address, balanceFormatted },
+    appState: { address, balanceFormatted, tranches },
   } = useAppState();
-  const totalTranches = 2;
   const unlockedBalance = 0.0005;
   const lockedBalance = 0.0005;
   const stakedBalance = 0.0005;
+  const userTranches =
+    tranches?.filter((t) => t.users.some(({ address: a }) => a === address)) ||
+    [];
 
+  if (!userTranches.length) {
+    return (
+      <section data-testid="redemption-page">
+        <div data-testid="redemption-no-balance">
+          You do not have any vesting VEGA tokens. Switch to another Ethereum
+          key to check what can be redeemed.
+        </div>
+      </section>
+    );
+  }
   return (
     <section data-testid="redemption-page">
       <div data-testid="redemption-description">
-        {address} has {balanceFormatted} VEGA tokens in {totalTranches} tranches
-        of the vesting contract.
+        {address} has {balanceFormatted} VEGA tokens in {userTranches.length}{" "}
+        tranches of the vesting contract.
       </div>
       <div data-testid="redemption-unlocked-tokens">
         A total of {unlockedBalance} Unlocked Vega tokens.
