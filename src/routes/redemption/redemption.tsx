5efdc50464699f528eb707c7030fa1636cc34707
Matthew Russell <mattrussell36@gmail.com>
2021-08-20 16:27:02 -0700

    remove check for tranches on redemption page

--- src/routes/redemption/redemption.tsx
+++ src/routes/redemption/redemption.tsx
@@ -23,52 +23,49 @@ const RedemptionRouter = ({
   );
   React.useEffect(() => {
     const run = async () => {
-      // Don't do anything until the user has connected
-      if (tranches) {
+      dispatch({
+        type: "SET_LOADING",
+        loading: true,
+      });
+      try {
+        const userTranches = tranches.filter((t) =>
+          t.users.some(
+            ({ address: a }) => a.toLowerCase() === address.toLowerCase()
+          )
+        );
+        dispatch({
+          type: "SET_USER_TRANCHES",
+          userTranches,
+        });
+        const getLien = vesting.getLien(address);
+        const promises = userTranches.map(async (t) => {
+          const [total, vested] = await Promise.all([
+            vesting.userTrancheTotalBalance(address, t.tranche_id),
+            vesting.userTrancheVestedBalance(address, t.tranche_id),
+          ]);
+          return {
+            id: t.tranche_id,
+            locked: total.minus(vested),
+            vested,
+          };
+        });
+        const balances = await Promise.all(promises);
+        const lien = await getLien;
+        dispatch({
+          type: "SET_USER_BALANCES",
+          balances,
+          lien,
+        });
+      } catch (e) {
+        dispatch({
+          type: "ERROR",
+          error: e,
+        });
+      } finally {
         dispatch({
           type: "SET_LOADING",
-          loading: true,
+          loading: false,
         });
-        try {
-          const userTranches = tranches.filter((t) =>
-            t.users.some(
-              ({ address: a }) => a.toLowerCase() === address.toLowerCase()
-            )
-          );
-          dispatch({
-            type: "SET_USER_TRANCHES",
-            userTranches,
-          });
-          const getLien = vesting.getLien(address);
-          const promises = userTranches.map(async (t) => {
-            const [total, vested] = await Promise.all([
-              vesting.userTrancheTotalBalance(address, t.tranche_id),
-              vesting.userTrancheVestedBalance(address, t.tranche_id),
-            ]);
-            return {
-              id: t.tranche_id,
-              locked: total.minus(vested),
-              vested,
-            };
-          });
-          const balances = await Promise.all(promises);
-          const lien = await getLien;
-          dispatch({
-            type: "SET_USER_BALANCES",
-            balances,
-            lien,
-          });
-        } catch (e) {
-          dispatch({
-            type: "ERROR",
-            error: e,
-          });
-        } finally {
-          dispatch({
-            type: "SET_LOADING",
-            loading: false,
-          });
-        }
       }
     };
     run();
