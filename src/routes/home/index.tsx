606b7213f1dfd5bb6bc7c12cd3782090217485fa
Matthew Russell <mattrussell36@gmail.com>
2021-08-23 11:26:53 -0700

    use tranche container

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -6,74 +6,72 @@ import { useDocumentTitle } from "../../hooks/use-document-title";
 import { TokenDetails } from "./token-details";
 import { StakingOverview } from "./staking-overview";
 import { Link } from "react-router-dom";
-import React from "react";
-import {
-  AppStateActionType,
-  useAppState,
-} from "../../contexts/app-state/app-state-context";
-import { useVegaVesting } from "../../hooks/use-vega-vesting";
+import { useAppState } from "../../contexts/app-state/app-state-context";
 import { Web3Container } from "../../components/web3-container";
 import { TemplateDefault } from "../../components/page-templates/template-default";
+import { TrancheContainer } from "../../components/tranche-container";
 
 const Home = ({ name }: RouteChildProps) => {
   useDocumentTitle(name);
 
   const { t } = useTranslation();
-  const { appState, appDispatch } = useAppState();
-  const vesting = useVegaVesting();
-
-  React.useEffect(() => {
-    const run = async () => {
-      const tranches = await vesting.getAllTranches();
-      appDispatch({ type: AppStateActionType.SET_TRANCHES, tranches });
-    };
-
-    run();
-  }, [appDispatch, vesting]);
+  const { appState } = useAppState();
 
   return (
     <TemplateDefault title={t("pageTitleHome")}>
       <Web3Container>
-        <h2>{t("The Vega Token")}</h2>
+        <TrancheContainer>
+          {() => (
+            <>
+              <h2>{t("The Vega Token")}</h2>
 
-        <TokenDetails />
+              <TokenDetails />
 
-        <h2>{t("Token Vesting")}</h2>
-        <p>
-          {t(
-            "Most VEGA tokens are held in a vesting contract. This means that they cannot be transferred between wallets until their vesting term is complete"
+              <h2>{t("Token Vesting")}</h2>
+              <p>
+                {t(
+                  "Most VEGA tokens are held in a vesting contract. This means that they cannot be transferred between wallets until their vesting term is complete"
+                )}
+              </p>
+              <p>
+                {t(
+                  "Tokens are held in different Tranches. Each tranche has its own schedule for how long the tokens are locked"
+                )}
+              </p>
+              <p>
+                {t(
+                  "Once tokens have unlocked they can be redeemed to the Ethereum wallet that owns them"
+                )}
+              </p>
+
+              <h2>{t("Governance")}</h2>
+              <p>
+                {t("Token holders can propose changes to the Vega network")}
+              </p>
+
+              <p>
+                <Link to={"/governance"}>
+                  {t("Read about Governance on Vega")}
+                </Link>
+              </p>
+
+              <h2>{t("Staking")}</h2>
+              <p>
+                {t(
+                  "Token holders can nominate their tokens to a validator and are rewarded a proportion of the fees accumulated for infrastructure"
+                )}
+              </p>
+
+              <StakingOverview
+                totalStakedFormatted={appState.totalStakedFormatted}
+              />
+
+              <p>
+                <Link to={"/staking"}>{t("Read about staking on Vega")}</Link>
+              </p>
+            </>
           )}
-        </p>
-        <p>
-          {t(
-            "Tokens are held in different Tranches. Each tranche has its own schedule for how long the tokens are locked"
-          )}
-        </p>
-        <p>
-          {t(
-            "Once tokens have unlocked they can be redeemed to the Ethereum wallet that owns them"
-          )}
-        </p>
-
-        <h2>{t("Governance")}</h2>
-        <p>{t("Token holders can propose changes to the Vega network")}</p>
-
-        <p>
-          <Link to={"/governance"}>{t("Read about Governance on Vega")}</Link>
-        </p>
-
-        <h2>{t("Staking")}</h2>
-        <p>
-          {t(
-            "Token holders can nominate their tokens to a validator and are rewarded a proportion of the fees accumulated for infrastructure"
-          )}
-        </p>
-
-        <StakingOverview totalStakedFormatted={appState.totalStakedFormatted} />
-
-        <p>
-          <Link to={"/staking"}>{t("Read about staking on Vega")}</Link>
-        </p>
+        </TrancheContainer>
       </Web3Container>
     </TemplateDefault>
   );
--- src/routes/home/staking-overview/staking-overview.tsx
+++ src/routes/home/staking-overview/staking-overview.tsx
@@ -1,16 +1,25 @@
-import React from "react";
 import { useTranslation } from "react-i18next";
 
-export const StakingOverview = ({ totalStakedFormatted }: { totalStakedFormatted: string }) => {
+export const StakingOverview = ({
+  totalStakedFormatted,
+}: {
+  totalStakedFormatted: string;
+}) => {
   const { t } = useTranslation();
 
   // TODO: Fetch values
   const nodeCount = 13;
   const sharedStake = totalStakedFormatted;
 
-  return (<p>
-    {t("There are {{nodeCount}} nodes with a shared stake of {{sharedStake}} VEGA tokens", {
-      nodeCount,
-      sharedStake
-    })}</p>);
-}
+  return (
+    <p>
+      {t(
+        "There are {{nodeCount}} nodes with a shared stake of {{sharedStake}} VEGA tokens",
+        {
+          nodeCount,
+          sharedStake,
+        }
+      )}
+    </p>
+  );
+};
