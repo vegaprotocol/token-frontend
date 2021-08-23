d36a87e09bc709e7442e2ac43c85665f78077be2
Matthew Russell <mattrussell36@gmail.com>
2021-08-23 16:54:41 -0700

    remove use of hardcode values and wrap pages with vega token container where required

--- src/lib/decimals.ts
+++ src/lib/decimals.ts
@@ -1,8 +1,10 @@
-import { BigNumber } from '../lib/bignumber'
-import {Decimals, EthereumChainIds} from "./web3-utils";
+import { BigNumber } from "../lib/bignumber";
 
-export function addDecimal(value: BigNumber, decimals: number = Decimals[EthereumChainIds.Mainnet]): string {
-  return value.dividedBy(Math.pow(10, decimals)).decimalPlaces(decimals).toString();
+export function addDecimal(value: BigNumber, decimals: number): string {
+  return value
+    .dividedBy(Math.pow(10, decimals))
+    .decimalPlaces(decimals)
+    .toString();
 }
 export function removeDecimal(value: BigNumber, decimals: number): string {
   return value.times(Math.pow(10, decimals)).toFixed(0);
