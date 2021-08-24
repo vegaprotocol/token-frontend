da5ac39e64371acf3c83b4ca3b2ca67c12b166fd
Dexter <dexter.edwards93@gmail.com>
2021-08-24 15:33:09 +0100

    starts adding tests

--- automation/cypress/integration/associate-vesting-tokens.js
+++ automation/cypress/integration/associate-vesting-tokens.js
@@ -1,8 +1,46 @@
-describe("associate - vesting tokens", () => {
-  it("disabled the button if amount is empty", () => {});
-  it("disabled the button if amount is greater than maximum", () => {});
-  it("disabled the button if amount is 0", () => {});
-  it("disabled the button if amount is less than 0", () => {});
-  it("calculates maximum correctly if some tokens are staked", () => {});
-  it("renders in progress and completed states", () => {});
+import { mock } from "../common/mock";
+
+describe("Associate - vesting tokens", () => {
+  it("Disabled the button if amount is empty", () => {
+    // As a user
+    mock(cy);
+    // When visiting the associate page
+    cy.visit("/associate?method=Contract");
+    // Then the button is disabled by default
+    cy.get('[data-testid="associate-button"]').should("be.disabled");
+  });
+
+  it("Disabled the button if amount is 0", () => {
+    // As a user
+    mock(cy);
+    // When visiting the associate page
+    cy.visit("/associate?method=Contract");
+    // Then the button is disabled by default
+    cy.get('[data-testid="associate-amount-input"]').type("0");
+    cy.get('[data-testid="associate-button"]').should("be.disabled");
+  });
+
+  it("Disabled the button if amount is less than 0", () => {
+    // As a user
+    mock(cy);
+    // When visiting the associate page
+    cy.visit("/associate?method=Contract");
+    // Then the button is disabled by default
+    cy.get('[data-testid="associate-amount-input"]').type("-1");
+    cy.get('[data-testid="associate-button"]').should("be.disabled");
+  });
+
+  // TODO needs wallet mocking
+
+  it.skip("Disabled the button if amount is greater than maximum", () => {
+    // As a user
+    mock(cy);
+    // When visiting the associate page
+    cy.visit("/associate?method=Contract");
+    // Then the button is disabled by default
+    cy.get('[data-testid="associate-amount-input"]').type("123");
+    cy.get('[data-testid="associate-button"]').should("be.disabled");
+  });
+  it.skip("Calculates maximum correctly if some tokens are staked", () => {});
+  it.skip("Renders in progress and completed states", () => {});
 });
