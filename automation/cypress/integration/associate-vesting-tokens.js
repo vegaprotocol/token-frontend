af95138a957ddc8824e9c4f4e26d9f08a50d791f
Dexter <dexter.edwards93@gmail.com>
2021-08-24 13:38:02 +0100

    add test skeletons

--- automation/cypress/integration/associate-vesting-tokens.js
+++ automation/cypress/integration/associate-vesting-tokens.js
@@ -1,1 +1,6 @@
-describe("associate - vesting tokens", () => {});
+describe("associate - vesting tokens", () => {
+  it("disabled the button if amount is empty", () => {});
+  it("disabled the button if amount is greater than maximum", () => {});
+  it("disabled the button if amount is 0", () => {});
+  it("disabled the button if amount is less than 0", () => {});
+});
