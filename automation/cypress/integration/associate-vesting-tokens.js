dd29663999add4b06e2edb25cd14ac40d9aeb832
Dexter <dexter.edwards93@gmail.com>
2021-08-24 14:11:22 +0100

    adds tx callout for testin

--- automation/cypress/integration/associate-vesting-tokens.js
+++ automation/cypress/integration/associate-vesting-tokens.js
@@ -3,4 +3,6 @@ describe("associate - vesting tokens", () => {
   it("disabled the button if amount is greater than maximum", () => {});
   it("disabled the button if amount is 0", () => {});
   it("disabled the button if amount is less than 0", () => {});
+  it("calculates maximum correctly if some tokens are staked", () => {});
+  it("renders in progress and completed states", () => {});
 });
