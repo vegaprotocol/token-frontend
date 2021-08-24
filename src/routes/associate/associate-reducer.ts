c7813d2e72bbdbf89eff449289465f1eddad200f
Dexter <dexter.edwards93@gmail.com>
2021-08-24 14:52:02 +0100

    push amount and hoist transaction state

--- src/routes/associate/associate-reducer.ts
+++ src/routes/associate/associate-reducer.ts
@@ -3,31 +3,44 @@ import { BigNumber } from "../../lib/bignumber";
 export interface AssociateState {
   error: Error | null;
   stakedBalance: BigNumber | null;
+  amount: string;
 }
 
 export const initialAssociateState: AssociateState = {
   error: null,
   stakedBalance: null,
+  amount: "",
 };
 
 export enum AssociateActionType {
   ERROR,
+  SET_AMOUNT,
 }
 
-export type RedemptionAction = {
-  type: AssociateActionType.ERROR;
-  error: Error;
-};
+export type AssociateAction =
+  | {
+      type: AssociateActionType.ERROR;
+      error: Error;
+    }
+  | {
+      type: AssociateActionType.SET_AMOUNT;
+      amount: string;
+    };
 
 export function associateReducer(
   state: AssociateState,
-  action: RedemptionAction
+  action: AssociateAction
 ): AssociateState {
   switch (action.type) {
     case AssociateActionType.ERROR:
       return {
         ...state,
         error: action.error,
+      };
+    case AssociateActionType.SET_AMOUNT:
+      return {
+        ...state,
+        amount: action.amount,
       };
   }
 }
--- src/routes/associate/contract-associate.tsx
+++ src/routes/associate/contract-associate.tsx
@@ -7,16 +7,28 @@ import React from "react";
 import { useVegaVesting } from "../../hooks/use-vega-vesting";
 import { useAppState } from "../../contexts/app-state/app-state-context";
 import { BigNumber } from "../../lib/bignumber";
-import { useTransaction } from "../../hooks/use-transaction";
-import { TransactionCallout } from "../../components/transaction-callout";
 import {
-  TransactionActionType,
-  TxState,
-} from "../../hooks/transaction-reducer";
+  AssociateAction,
+  AssociateActionType,
+  AssociateState,
+} from "./associate-reducer";
 
-export const ContractAssociate = () => {
+const inputName = "amount";
+
+export const ContractAssociate = ({
+  perform,
+  state,
+  dispatch,
+}: {
+  perform: () => void;
+  state: AssociateState;
+  dispatch: React.Dispatch<AssociateAction>;
+}) => {
   const { t } = useTranslation();
-  const [amount, setAmount] = React.useState<string | undefined>("");
+  const { amount } = state;
+  const setAmount = React.useCallback((value: string) => {
+    dispatch({ type: AssociateActionType.SET_AMOUNT, amount: value });
+  }, [dispatch]);
   const [stakedBalance, setStakedBalance] = React.useState<BigNumber>(
     new BigNumber("0")
   );
@@ -24,6 +36,7 @@ export const ContractAssociate = () => {
     appState: { currVegaKey, address, balanceFormatted },
   } = useAppState();
   const vesting = useVegaVesting();
+
   React.useEffect(() => {
     const run = async () => {
       if (currVegaKey && address) {
@@ -36,27 +49,20 @@ export const ContractAssociate = () => {
     };
     run();
   }, [address, currVegaKey, vesting]);
+
   const maximum = React.useMemo(() => {
     return new BigNumber(balanceFormatted).minus(stakedBalance).toString();
   }, [balanceFormatted, stakedBalance]);
   const useMaximum = React.useCallback(() => {
     setAmount(maximum);
   }, [setAmount, maximum]);
-  const inputName = "amount";
+
   const isDisabled = React.useMemo<boolean>(
     () =>
       !amount ||
       new BigNumber(amount).isLessThanOrEqualTo("0") ||
       new BigNumber(amount).isGreaterThan(maximum),
     [amount, maximum]
-  );
-  const {
-    state: txState,
-    dispatch: txDispatch,
-    perform,
-  } = useTransaction(
-    () => vesting.addStake(address!, amount!, currVegaKey!.pub),
-    () => vesting.checkAddStake(address!, amount!, currVegaKey!.pub)
   );
 
   return (
@@ -112,30 +118,6 @@ export const ContractAssociate = () => {
       >
         {t("Associate VEGA Tokens with key")}
       </Button>
-      {txState.txState !== TxState.Default ? (
-        <TransactionCallout
-          completeHeading={t("Done")}
-          completeBody={t(
-            "Vega key {{vegaKey}} can now participate in governance and Nominate a validator with it’s stake.",
-            { vegaKey: currVegaKey?.pub }
-          )}
-          completeFooter={
-            <button style={{ width: "100%" }}>
-              {t("Nominate Stake to Validator Node")}
-            </button>
-          }
-          pendingHeading={t("Associating Tokens")}
-          pendingBody={t(
-            "Associating {{amount}} VEGA tokens with Vega key {{vegaKey}}",
-            { amount, vegaKey: currVegaKey?.pub }
-          )}
-          pendingFooter={t(
-            "The Vega network requires 30 Confirmations (approx 5 minutes) on Ethereum before crediting your Vega key with your tokens. This page will update once complete or you can come back and check your Vega wallet to see if it is ready to use."
-          )}
-          state={txState}
-          reset={() => txDispatch({ type: TransactionActionType.TX_RESET })}
-        />
-      ) : null}
     </section>
   );
 };
