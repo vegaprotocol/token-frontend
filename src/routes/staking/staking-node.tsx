552f744e120b65fd82caf74f5ebe0eadd146f197
Matthew Russell <mattrussell36@gmail.com>
2021-08-25 15:35:39 -0700

    make title h2

--- src/routes/staking/staking-node.tsx
+++ src/routes/staking/staking-node.tsx
@@ -23,7 +23,7 @@ export const StakingNode = ({ vegaKey }: StakingNodeProps) => {
 
   return (
     <>
-      <h1>{t("VALIDATOR {{node}}", { node })}</h1>
+      <h2>{t("VALIDATOR {{node}}", { node })}</h2>
       <p>Vega key: {vegaKey.pubShort}</p>
       <ValidatorTable node={node} />
       <EpochCountdown
