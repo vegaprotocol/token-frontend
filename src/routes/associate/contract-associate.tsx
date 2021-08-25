56af2c7e0ce8c600f43b5b3d0ad3815f45209715
Dexter <dexter.edwards93@gmail.com>
2021-08-25 12:28:58 +0100

    move form into its' own component

--- src/routes/associate/contract-associate.tsx
+++ src/routes/associate/contract-associate.tsx
@@ -1,18 +1,10 @@
-import "./contract-associate.scss";
-import { Button, FormGroup, InputGroup, Intent, Tag } from "@blueprintjs/core";
 import { useTranslation } from "react-i18next";
-import { Colors } from "../../colors";
 import { Callout } from "../../components/callout";
 import React from "react";
 import { useAppState } from "../../contexts/app-state/app-state-context";
 import { BigNumber } from "../../lib/bignumber";
-import {
-  AssociateAction,
-  AssociateActionType,
-  AssociateState,
-} from "./associate-reducer";
-
-const inputName = "amount";
+import { AssociateAction, AssociateState } from "./associate-reducer";
+import { AssociateFrom } from "./associate-form";
 
 export const ContractAssociate = ({
   perform,
@@ -23,28 +15,14 @@ export const ContractAssociate = ({
   state: AssociateState;
   dispatch: React.Dispatch<AssociateAction>;
 }) => {
-  const { amount } = state;
   const { t } = useTranslation();
-  const setAmount = React.useCallback(
-    (value: string) => {
-      dispatch({ type: AssociateActionType.SET_AMOUNT, amount: value });
-    },
-    [dispatch]
-  );
   const {
     appState: { currVegaKey, balanceFormatted, lien },
   } = useAppState();
 
   const maximum = React.useMemo(() => {
-    return new BigNumber(balanceFormatted).minus(lien!).toString();
+    return new BigNumber(balanceFormatted).minus(lien!);
   }, [balanceFormatted, lien]);
-  const isDisabled = React.useMemo<boolean>(
-    () =>
-      !amount ||
-      new BigNumber(amount).isLessThanOrEqualTo("0") ||
-      new BigNumber(amount).isGreaterThan(maximum),
-    [amount, maximum]
-  );
 
   return (
     <section className="contract-associate" data-testid="contract-associate">
@@ -53,52 +31,14 @@ export const ContractAssociate = ({
           "You can associate tokens while they are held in the vesting contract, when they unlock you will need to dissociate them before they can be redeemed."
         )}
       </Callout>
-      <h2 data-testid="associate-vega-key-header">
-        {t("What Vega wallet/key is going to control your stake?")}
-      </h2>
-      <strong
-        data-testid="associate-vega-key-label"
-        style={{ color: Colors.WHITE }}
-      >
-        {t("Connected Vega key")}
-      </strong>
-      <p style={{ color: Colors.WHITE }} data-testid="associate-vega-key">
-        {currVegaKey?.pub}
-      </p>
-      <h2 data-testid="associate-amount-header">
-        {t("How much would you like to associate?")}
-      </h2>
-      <FormGroup label="" labelFor={inputName}>
-        <div style={{ display: "flex" }}>
-          <InputGroup
-            data-testid="associate-amount-input"
-            className="contract-associate__input"
-            name={inputName}
-            id={inputName}
-            onChange={(e) => setAmount(e.target.value)}
-            value={amount}
-            intent={Intent.NONE}
-            rightElement={<Tag minimal={true}>{t("VEGA Tokens")}</Tag>}
-            autoComplete="off"
-            type="number"
-          />
-          <button
-            onClick={() => setAmount(maximum)}
-            data-testid="associate-amount-use-maximum"
-            className="button-link contract-associate__use-maximum "
-          >
-            {t("Use maximum")}
-          </button>
-        </div>
-      </FormGroup>
-      <Button
-        data-testid="associate-button"
-        fill={true}
-        disabled={isDisabled}
-        onClick={perform}
-      >
-        {t("Associate VEGA Tokens with key")}
-      </Button>
+      <AssociateFrom
+        state={state}
+        maximum={maximum}
+        dispatch={dispatch}
+        pubKey={currVegaKey!.pub}
+        perform={perform}
+        requireApprove={false}
+      />
     </section>
   );
 };
