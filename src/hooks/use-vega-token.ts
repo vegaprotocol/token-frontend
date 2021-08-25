8ac0795016108bd2c24d0ce47b15bf0fceb8f2c6
Dexter <dexter.edwards93@gmail.com>
2021-08-25 09:26:11 +0100

    ensure hook returns type for better type checking

--- src/hooks/use-vega-token.ts
+++ src/hooks/use-vega-token.ts
@@ -3,6 +3,7 @@ import Web3 from "web3";
 import { useAppState } from "../contexts/app-state/app-state-context";
 // @ts-ignore VEGA_WEB3 path swapped depending on prod build or test build
 import VegaToken from "../lib/VEGA_WEB3/vega-token";
+import { IVegaToken } from "../lib/web3-utils";
 
 export function useVegaToken() {
   const {
@@ -10,7 +11,7 @@ export function useVegaToken() {
     appState: { contractAddresses },
   } = useAppState();
 
-  return React.useMemo(() => {
+  return React.useMemo<IVegaToken>(() => {
     const web3 = new Web3(provider);
     return new VegaToken(web3, contractAddresses.vegaTokenAddress);
   }, [contractAddresses.vegaTokenAddress, provider]);
