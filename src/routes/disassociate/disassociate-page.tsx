8ef7f723642802acae5bb871d5d11909448b9277
Dexter <dexter.edwards93@gmail.com>
2021-08-26 12:04:45 +0100

    seperate files for components
    Cleanup on isle 4!

--- src/routes/disassociate/disassociate-page.tsx
+++ src/routes/disassociate/disassociate-page.tsx
@@ -1,5 +1,4 @@
 import "./disassociate-page.scss";
-import BigNumber from "bignumber.js";
 import React, { useReducer } from "react";
 import { useTranslation } from "react-i18next";
 import { ConnectedVegaKey } from "../../components/connected-vega-key";
@@ -7,11 +6,7 @@ import {
   StakingMethod,
   StakingMethodRadio,
 } from "../../components/staking-method-radio";
-import { TokenInput } from "../../components/token-input";
-import {
-  useAppState,
-  VegaKeyExtended,
-} from "../../contexts/app-state/app-state-context";
+import { VegaKeyExtended } from "../../contexts/app-state/app-state-context";
 import { useSearchParams } from "../../hooks/use-search-params";
 import {
   DisassociateActionType,
@@ -21,155 +16,10 @@ import {
 import { useTransaction } from "../../hooks/use-transaction";
 import { useVegaVesting } from "../../hooks/use-vega-vesting";
 import { useVegaStaking } from "../../hooks/use-vega-staking";
-import {
-  TransactionAction,
-  TransactionActionType,
-  TransactionState,
-  TxState,
-} from "../../hooks/transaction-reducer";
-import { TransactionCallout } from "../../components/transaction-callout";
-import { useHistory } from "react-router-dom";
-
-const WalletDisassociate = ({
-  perform,
-  amount,
-  setAmount,
-}: {
-  perform: () => void;
-  amount: string;
-  setAmount: React.Dispatch<string>;
-}) => {
-  const {
-    appState: { vegaStakedBalance },
-  } = useAppState();
-  const { t } = useTranslation();
-  const maximum = React.useMemo(
-    () => new BigNumber(vegaStakedBalance!),
-    [vegaStakedBalance]
-  );
-  const isDisabled = React.useMemo<boolean>(
-    () =>
-      !amount ||
-      new BigNumber(amount).isLessThanOrEqualTo("0") ||
-      new BigNumber(amount).isGreaterThan(maximum),
-    [amount, maximum]
-  );
-
-  if (new BigNumber(vegaStakedBalance!).isEqualTo("0")) {
-    return (
-      <div className="disassociate-page__error">
-        {t(
-          "You have no VEGA tokens currently staked through your connected Vega wallet."
-        )}
-      </div>
-    );
-  }
-
-  return (
-    <>
-      <TokenInput maximum={maximum} amount={amount} setAmount={setAmount} />
-      <button
-        style={{ marginTop: 10, width: "100%" }}
-        data-testid="disassociate-button"
-        disabled={isDisabled}
-        onClick={perform}
-      >
-        {t("Disassociate VEGA Tokens from key")}
-      </button>
-    </>
-  );
-};
-
-const ContractDisassociate = ({
-  perform,
-  amount,
-  setAmount,
-}: {
-  perform: () => void;
-  amount: string;
-  setAmount: React.Dispatch<string>;
-}) => {
-  const {
-    appState: { lien },
-  } = useAppState();
-  const { t } = useTranslation();
-  const maximum = React.useMemo(() => new BigNumber(lien), [lien]);
-  const isDisabled = React.useMemo<boolean>(
-    () =>
-      !amount ||
-      new BigNumber(amount).isLessThanOrEqualTo("0") ||
-      new BigNumber(amount).isGreaterThan(maximum),
-    [amount, maximum]
-  );
-  if (new BigNumber(lien).isEqualTo("0")) {
-    return (
-      <div className="disassociate-page__error">
-        {t(
-          "You have no VEGA tokens currently staked through your connected Eth wallet."
-        )}
-      </div>
-    );
-  }
-  return (
-    <>
-      <TokenInput maximum={maximum} amount={amount} setAmount={setAmount} />
-      <button
-        style={{ marginTop: 10, width: "100%" }}
-        data-testid="disassociate-button"
-        disabled={isDisabled}
-        onClick={perform}
-      >
-        {t("Disassociate VEGA Tokens from key")}
-      </button>
-    </>
-  );
-};
-
-const DisassociateCallout = ({
-  amount,
-  vegaKey,
-  state,
-  dispatch,
-  stakingMethod,
-}: {
-  amount: string;
-  vegaKey: string;
-  state: TransactionState;
-  dispatch: React.Dispatch<TransactionAction>;
-  stakingMethod: StakingMethod;
-}) => {
-  const { t } = useTranslation();
-  const history = useHistory();
-  return (
-    <TransactionCallout
-      completeHeading={t("Done")}
-      completeBody={
-        stakingMethod === StakingMethod.Contract
-          ? t("{{amount}} VEGA tokens have been returned to Vesting contract", {
-              amount,
-            })
-          : t("{{amount}} VEGA tokens have been returned to Ethereum wallet", {
-              amount,
-            })
-      }
-      completeFooter={
-        <button
-          style={{ width: "100%" }}
-          onClick={() => history.push("/redemption")}
-        >
-          {t("Redeem tokens")}
-        </button>
-      }
-      pendingHeading={t("Dissociating Tokens")}
-      pendingBody={t(
-        "Dissociating  {{amount}} VEGA tokens from Vega key {{vegaKey}}",
-        { amount, vegaKey }
-      )}
-      state={state}
-      reset={() => dispatch({ type: TransactionActionType.TX_RESET })}
-    />
-  );
-};
+import { TxState } from "../../hooks/transaction-reducer";
+import { WalletDisassociate } from "./wallet-disassociate";
+import { ContractDisassociate } from "./contract-disassociate";
+import { DisassociateTransaction } from "./disassociate-transaction";
 
 export const DisassociatePage = ({
   address,
@@ -216,7 +66,7 @@ export const DisassociatePage = ({
   >(stakingMethod);
   if (vestingBridgeTx.txState !== TxState.Default) {
     return (
-      <DisassociateCallout
+      <DisassociateTransaction
         state={vestingBridgeTx}
         amount={amount}
         vegaKey={vegaKey.pub}
@@ -226,7 +76,7 @@ export const DisassociatePage = ({
     );
   } else if (walletTx.txState !== TxState.Default) {
     return (
-      <DisassociateCallout
+      <DisassociateTransaction
         state={walletTx}
         amount={amount}
         vegaKey={vegaKey.pub}
---
+++ src/routes/disassociate/disassociate-transaction.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { StakingMethod } from "../../components/staking-method-radio";
+import {
+  TransactionAction,
+  TransactionActionType,
+  TransactionState,
+} from "../../hooks/transaction-reducer";
+import { TransactionCallout } from "../../components/transaction-callout";
+import { useHistory } from "react-router-dom";
+
+export const DisassociateTransaction = ({
+  amount,
+  vegaKey,
+  state,
+  dispatch,
+  stakingMethod,
+}: {
+  amount: string;
+  vegaKey: string;
+  state: TransactionState;
+  dispatch: React.Dispatch<TransactionAction>;
+  stakingMethod: StakingMethod;
+}) => {
+  const { t } = useTranslation();
+  const history = useHistory();
+  return (
+    <TransactionCallout
+      completeHeading={t("Done")}
+      completeBody={
+        stakingMethod === StakingMethod.Contract
+          ? t("{{amount}} VEGA tokens have been returned to Vesting contract", {
+              amount,
+            })
+          : t("{{amount}} VEGA tokens have been returned to Ethereum wallet", {
+              amount,
+            })
+      }
+      completeFooter={
+        <button
+          style={{ width: "100%" }}
+          onClick={() => history.push("/redemption")}
+        >
+          {t("Redeem tokens")}
+        </button>
+      }
+      pendingHeading={t("Dissociating Tokens")}
+      pendingBody={t(
+        "Dissociating  {{amount}} VEGA tokens from Vega key {{vegaKey}}",
+        { amount, vegaKey }
+      )}
+      state={state}
+      reset={() => dispatch({ type: TransactionActionType.TX_RESET })}
+    />
+  );
+};
