8adb60cbf930e0cabddb45c7b9fc56518344b3b0
Dexter <dexter.edwards93@gmail.com>
2021-08-27 08:25:05 +0100

    create hook for refreshing tranche balances

---
+++ src/hooks/use-get-user-tranche-balances.ts
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  AppStateActionType,
+  useAppState,
+} from "../contexts/app-state/app-state-context";
+import { useVegaVesting } from "./use-vega-vesting";
+
+export const useGetUserTrancheBalances = (address: string) => {
+  const vesting = useVegaVesting();
+  const { appDispatch } = useAppState();
+  return React.useCallback(async () => {
+    const tranches = await vesting.getAllTranches();
+    const userTranches = tranches.filter((t) =>
+      t.users.some(
+        ({ address: a }) => a.toLowerCase() === address.toLowerCase()
+      )
+    );
+    const promises = userTranches.map(async (t) => {
+      const [total, vested] = await Promise.all([
+        vesting.userTrancheTotalBalance(address, t.tranche_id),
+        vesting.userTrancheVestedBalance(address, t.tranche_id),
+      ]);
+      return {
+        id: t.tranche_id,
+        locked: total.minus(vested),
+        vested,
+      };
+    });
+    const trancheBalances = await Promise.all(promises);
+    appDispatch({
+      type: AppStateActionType.SET_TRANCHE_DATA,
+      trancheBalances,
+      tranches,
+    });
+  }, [address, appDispatch, vesting]);
+};
