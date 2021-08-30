a9925ec8ab0994e80a399d7358b9265e9273ed32
Dexter <dexter.edwards93@gmail.com>
2021-08-30 13:40:57 +0100

    fix home page tests

--- automation/cypress/integration/home.js
+++ automation/cypress/integration/home.js
@@ -1,19 +1,12 @@
 import { mock } from "../common/mock";
 
-describe("Home", () => {
+describe.only("Home", () => {
   it("Displays the expected information", () => {
     mock(cy);
     cy.visit("/");
-    cy.contains("Loading");
-    cy.contains("The Vega Token");
-    cy.get('[data-testid="token-address"]').should(
-      "have.text",
-      "0xFa521aDcc11925266A518CdE33386FcD9cF2A4A6"
-    );
-    cy.get('[data-testid="token-contract"]').should(
-      "have.text",
-      "0xfc9Ad8fE9E0b168999Ee7547797BC39D55d607AA"
-    );
+    cy.contains("Connect to an Ethereum wallet").click();
+    cy.get('[data-testid="token-address"]').should("have.text", "0xFa52…A4A6");
+    cy.get('[data-testid="token-contract"]').should("have.text", "0xfc9A…07AA");
     cy.get('[data-testid="total-supply"]').should("have.text", "1000000000");
     cy.get('[data-testid="staked"]').should("have.text", "0.0004");
 
