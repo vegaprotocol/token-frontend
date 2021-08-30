9261341efa78179ea2b631a431271e699c5838f6
Matthew Russell <mattrussell36@gmail.com>
2021-08-30 15:01:09 -0700

    pass in data to validators table

--- src/routes/staking/__generated__/StakeNode.ts
+++ src/routes/staking/__generated__/StakeNode.ts
@@ -6,10 +6,10 @@
 import { NodeStatus } from "./../../../../__generated__/globalTypes";
 
 // ====================================================
-// GraphQL query operation: Node
+// GraphQL query operation: StakeNode
 // ====================================================
 
-export interface Node_node_epochData {
+export interface StakeNode_node_epochData {
   __typename: "EpochData";
   /**
    * Total number of epochs since node was created
@@ -25,7 +25,7 @@ export interface Node_node_epochData {
   online: number;
 }
 
-export interface Node_node {
+export interface StakeNode_node {
   __typename: "Node";
   /**
    * The node url eg n01.vega.xyz
@@ -59,11 +59,11 @@ export interface Node_node {
    * Amount of stake on the next epoch
    */
   pendingStake: string;
-  epochData: Node_node_epochData | null;
+  epochData: StakeNode_node_epochData | null;
   status: NodeStatus;
 }
 
-export interface Node_epoch_timestamps {
+export interface StakeNode_epoch_timestamps {
   __typename: "EpochTimestamps";
   /**
    * RFC3339 timestamp - Vega time of epoch start, null if not started
@@ -75,7 +75,7 @@ export interface Node_epoch_timestamps {
   end: string | null;
 }
 
-export interface Node_epoch {
+export interface StakeNode_epoch {
   __typename: "Epoch";
   /**
    * Presumably this is an integer or something. If there's no such thing, disregard
@@ -84,20 +84,32 @@ export interface Node_epoch {
   /**
    * Timestamps for start/end etc
    */
-  timestamps: Node_epoch_timestamps;
+  timestamps: StakeNode_epoch_timestamps;
 }
 
-export interface Node {
+export interface StakeNode_nodeData {
+  __typename: "NodeData";
+  /**
+   * Total staked amount across all nodes
+   */
+  stakedTotal: string;
+}
+
+export interface StakeNode {
   /**
    * specific node in network
    */
-  node: Node_node | null;
+  node: StakeNode_node | null;
   /**
    * get data for a specific epoch, if id omitted it gets the current epoch
    */
-  epoch: Node_epoch;
+  epoch: StakeNode_epoch;
+  /**
+   * returns information about nodes
+   */
+  nodeData: StakeNode_nodeData | null;
 }
 
-export interface NodeVariables {
+export interface StakeNodeVariables {
   nodeId: string;
 }
