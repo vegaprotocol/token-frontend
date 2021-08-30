9c9bfd287734bac53c8a5821d4ea38e825c4012d
Matthew Russell <mattrussell36@gmail.com>
2021-08-30 10:23:58 -0700

    add commandsync method for wallet service

--- src/lib/vega-wallet/vega-wallet-service.ts
+++ src/lib/vega-wallet/vega-wallet-service.ts
@@ -6,6 +6,7 @@ const Endpoints = {
   STATUS: "status",
   TOKEN: "auth/token",
   KEYS: "keys",
+  COMMAND: "command/sync",
 };
 
 const Errors = {
@@ -13,7 +14,17 @@ const Errors = {
   SERVICE_UNAVAILABLE: "Wallet service unavailable",
   SESSION_EXPIRED: "Session expired",
   INVALID_CREDENTIALS: "Invalid credentials",
+  COMMAND_FAILED: "Command failed",
 };
+
+export interface DelegateSubmissionInput {
+  pubKey: string;
+  delegateSubmission: {
+    nodeId: string;
+  };
+}
+
+export type CommandSyncInput = DelegateSubmissionInput;
 
 export interface IVegaWalletService {
   url: string;
@@ -116,6 +127,39 @@ export class VegaWalletService implements IVegaWalletService {
       const json = await res.json();
 
       return [undefined, json.keys];
+    } catch (err) {
+      return [Errors.SERVICE_UNAVAILABLE, undefined];
+    }
+  }
+
+  async commandSync(body: CommandSyncInput) {
+    if (!this.token) {
+      return [Errors.NO_TOKEN, undefined];
+    }
+
+    try {
+      const res = await fetch(`${this.url}/${Endpoints.COMMAND}`, {
+        method: "post",
+        body: JSON.stringify({
+          ...body,
+          propagate: true,
+        }),
+        headers: { authorization: `Bearer ${this.token}` },
+      });
+
+      // forbidden, clear token
+      if (res.status === 403) {
+        this.clearToken();
+        return [Errors.SESSION_EXPIRED, undefined];
+      }
+
+      const json = await res.json();
+
+      if ("error" in json) {
+        return [Errors.COMMAND_FAILED];
+      } else {
+        return [undefined, json];
+      }
     } catch (err) {
       return [Errors.SERVICE_UNAVAILABLE, undefined];
     }
