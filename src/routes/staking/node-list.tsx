a36fe03b8aa7421b794fe485ebd0a4fe2669abb5
Matthew Russell <mattrussell36@gmail.com>
2021-08-30 14:46:36 -0700

    list queries to top page

--- src/routes/staking/node-list.tsx
+++ src/routes/staking/node-list.tsx
@@ -1,108 +1,31 @@
-import React from "react";
-import { gql, useQuery } from "@apollo/client";
-import { Link, useRouteMatch } from "react-router-dom";
 import "./node-list.scss";
-import {
-  Staking,
-  Staking_nodeData,
-  Staking_nodes,
-} from "./__generated__/Staking";
-import { BigNumber } from "../../lib/bignumber";
-import { useAppState } from "../../contexts/app-state/app-state-context";
+import { Link, useRouteMatch } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
-const NODES_QUERY = gql`
-  query Staking($partyId: ID!) {
-    party(id: $partyId) {
-      id
-      delegations {
-        amount
-        node
-      }
-    }
-    nodes {
-      id
-      pubkey
-      infoUrl
-      location
-      stakedByOperator
-      stakedByDelegates
-      stakedTotal
-      pendingStake
-      epochData {
-        total
-        offline
-        online
-      }
-      status
-    }
-    nodeData {
-      stakedTotal
-      totalNodes
-      inactiveNodes
-      validatingNodes
-      uptime
-    }
-  }
-`;
+interface NodeListProps {
+  nodes: NodeListItemProps[];
+}
 
-export const NodeList = () => {
-  const { appState } = useAppState();
-
-  const { data, loading } = useQuery<Staking>(NODES_QUERY, {
-    variables: { partyId: appState.currVegaKey?.pub || "" },
-    skip: !appState.currVegaKey?.pub,
-  });
-
-  const nodes = React.useMemo<NodeListItemProps[]>(() => {
-    if (!data?.nodes) return [];
-
-    return data.nodes.map((node) => {
-      const stakedTotal = new BigNumber(data?.nodeData?.stakedTotal || 0);
-      const stakedOnNode = new BigNumber(node.stakedTotal);
-      const stakedTotalPercentage =
-        stakedTotal.isEqualTo(0) || stakedOnNode.isEqualTo(0)
-          ? "-"
-          : stakedOnNode.dividedBy(stakedTotal).times(100).toString() + "%";
-
-      const userStake = data.party?.delegations?.length
-        ? data.party?.delegations
-            ?.filter((d) => d.node === node.id)
-            .reduce((sum, d) => {
-              const value = new BigNumber(d.amount);
-              return sum.plus(value);
-            }, new BigNumber(0))
-        : new BigNumber(0);
-
-      const userStakePercentage =
-        userStake.isEqualTo(0) || stakedOnNode.isEqualTo(0)
-          ? "-"
-          : userStake.dividedBy(stakedOnNode).times(100).toString() + "%";
-
-      return {
-        id: node.id,
-        stakedTotal: stakedTotal.toString(),
-        stakedTotalPercentage,
-        userStake: userStake.toString(),
-        userStakePercentage,
-      };
-    });
-  }, [data]);
-
-  if (loading || !data?.nodes) {
-    return <div>Loading...</div>;
-  }
-
+export const NodeList = ({ nodes }: NodeListProps) => {
   return (
     <ul className="node-list">
-      {nodes.map((n, i) => {
+      {[
+        {
+          id: "foo",
+          stakedTotal: "100",
+          stakedTotalPercentage: "34%",
+          userStake: "10",
+          userStakePercentage: "1%",
+        },
+        ...nodes,
+      ].map((n, i) => {
         return <NodeListItem key={i} {...n} />;
       })}
     </ul>
   );
 };
 
-interface NodeListItemProps {
+export interface NodeListItemProps {
   id: string;
   stakedTotal: string;
   stakedTotalPercentage: string;
--- src/routes/staking/staking-node.tsx
+++ src/routes/staking/staking-node.tsx
@@ -7,32 +7,82 @@ import { VegaKeyExtended } from "../../contexts/app-state/app-state-context";
 import { EpochCountdown } from "./epoch-countdown";
 import { YourStake } from "./your-stake";
 import { StakingForm } from "./staking-form";
+import { gql, useQuery } from "@apollo/client";
+import { Node, NodeVariables } from "./__generated__/Node";
+import { Callout } from "../../components/callout";
+import { SplashScreen } from "../../components/splash-screen";
+import { SplashLoader } from "../../components/splash-loader";
+
+const NODE_QUERY = gql`
+  query Node($nodeId: String!) {
+    node(id: $nodeId) {
+      id
+      pubkey
+      infoUrl
+      location
+      stakedByOperator
+      stakedByDelegates
+      stakedTotal
+      pendingStake
+      epochData {
+        total
+        offline
+        online
+      }
+      status
+    }
+    epoch {
+      id
+      timestamps {
+        start
+        end
+      }
+    }
+  }
+`;
 
 interface StakingNodeProps {
   vegaKey: VegaKeyExtended;
 }
 
 export const StakingNode = ({ vegaKey }: StakingNodeProps) => {
-  // TODO temp data
-  const epochData = {
-    count: 1,
-    startDate: new Date(1626375300000),
-    endDate: new Date(1629885333607),
-  };
-  const { t } = useTranslation();
   const { node } = useParams<{ node: string }>();
+  const { t } = useTranslation();
+  const { data, loading, error } = useQuery<Node, NodeVariables>(NODE_QUERY, {
+    variables: { nodeId: node },
+    skip: !node,
+  });
+
+  if (error) {
+    return (
+      <Callout intent="error" title={t("Something went wrong")}>
+        <p>Query for node {node} failed.</p>
+        <pre>{error.message}</pre>
+      </Callout>
+    );
+  }
+
+  if (loading || !data) {
+    return (
+      <SplashScreen>
+        <SplashLoader />
+      </SplashScreen>
+    );
+  }
 
   return (
     <>
       <h2>{t("VALIDATOR {{node}}", { node })}</h2>
       <p>Vega key: {vegaKey.pubShort}</p>
       <ValidatorTable node={node} />
-      <EpochCountdown
-        containerClass="staking-node__epoch"
-        count={epochData.count}
-        startDate={epochData.startDate}
-        endDate={epochData.endDate}
-      />
+      {data.epoch.timestamps.start && data.epoch.timestamps.end && (
+        <EpochCountdown
+          containerClass="staking-node__epoch"
+          id={data.epoch.id}
+          startDate={new Date(data.epoch.timestamps.start)}
+          endDate={new Date(data.epoch.timestamps.end)}
+        />
+      )}
       <YourStake node={node} />
       <StakingForm />
     </>
