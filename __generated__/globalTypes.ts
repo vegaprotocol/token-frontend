bf54bc4a84e52cf4fe1203645172d0be66386eca
Dexter <dexter.edwards93@gmail.com>
2021-08-31 14:46:02 +0100

    generate

---
+++ __generated__/globalTypes.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export enum NodeStatus {
+  NonValidator = "NonValidator",
+  Validator = "Validator",
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
