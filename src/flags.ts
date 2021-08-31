944315f72d7050b9aa8f9edde2ab9f843bc67093
Dexter <dexter.edwards93@gmail.com>
2021-08-31 15:17:43 +0100

    chore add mainnet live flag

--- src/flags.ts
+++ src/flags.ts
@@ -1,6 +1,9 @@
+const TRUTHY = ["1", "true"];
+
 export const Flags = {
-  MOCK: ["1", "true"].includes(process.env.REACT_APP_MOCKED!),
-  SHOW_NETWORK_SWITCHER: ["1", "true"].includes(
+  MOCK: TRUTHY.includes(process.env.REACT_APP_MOCKED!),
+  MAINNET_LIVE: TRUTHY.includes(process.env.REACT_MAINNET_LIVE!),
+  SHOW_NETWORK_SWITCHER: TRUTHY.includes(
     process.env.REACT_APP_SHOW_NETWORK_SWITCHER || ""
   ),
 };
