9ff6f9568f1990bbcccf4977f63d45fb5b8a369c
Dexter <dexter.edwards93@gmail.com>
2021-09-01 17:02:59 +0100

    add shim

--- src/hooks/use-vega-staking.ts
+++ src/hooks/use-vega-staking.ts
@@ -1,9 +1,56 @@
 import React from "react";
 import Web3 from "web3";
 import { useAppState } from "../contexts/app-state/app-state-context";
-import { IVegaStaking } from "../lib/web3-utils";
+import { IStaking, IVegaStaking, PromiEvent } from "../lib/web3-utils";
 // @ts-ignore
 import StakingAbi from "../lib/VEGA_WEB3/vega-staking";
+import BigNumber from "bignumber.js";
+import { Flags } from "../flags";
+
+class StakingShim implements IVegaStaking {
+  checkTransferStake(
+    address: string,
+    amount: string,
+    newAddress: string,
+    vegaKey: string
+  ): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  transferStake(
+    address: string,
+    amount: string,
+    newAddress: string,
+    vegaKey: string
+  ): PromiEvent {
+    throw new Error("Method not implemented.");
+  }
+  async stakeBalance(address: string, vegaKey: string): Promise<BigNumber> {
+    return new BigNumber(0);
+  }
+  async totalStaked(): Promise<BigNumber> {
+    return new BigNumber(0);
+  }
+  removeStake(address: string, amount: string, vegaKey: string): PromiEvent {
+    throw new Error("Method not implemented.");
+  }
+  checkRemoveStake(
+    address: string,
+    amount: string,
+    vegaKey: string
+  ): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  addStake(address: string, amount: string, vegaKey: string): PromiEvent {
+    throw new Error("Method not implemented.");
+  }
+  checkAddStake(
+    address: string,
+    amount: string,
+    vegaKey: string
+  ): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+}
 
 export const useVegaStaking = () => {
   const {
@@ -12,6 +59,9 @@ export const useVegaStaking = () => {
   } = useAppState();
 
   return React.useMemo<IVegaStaking>(() => {
+    if (Flags.MAINNET_DISABLED) {
+      return new StakingShim();
+    }
     const web3 = new Web3(provider);
     return new StakingAbi(web3, contractAddresses.stakingBridge, decimals);
   }, [contractAddresses.stakingBridge, provider, decimals]);
