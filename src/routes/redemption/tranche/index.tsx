8702d4372449b7fd3824415b1bb859c3cdc343a3
Dexter <dexter.edwards93@gmail.com>
2021-09-02 11:07:59 +0100

    transaction chanegs

--- src/routes/redemption/tranche/index.tsx
+++ src/routes/redemption/tranche/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { Colors } from "../../../colors";
 import { TransactionCallout } from "../../../components/transaction-callout";
 import { useAppState } from "../../../contexts/app-state/app-state-context";
 import {
@@ -12,6 +13,7 @@ import { useRefreshBalances } from "../../../hooks/use-refresh-balances";
 import { useTransaction } from "../../../hooks/use-transaction";
 import { useVegaVesting } from "../../../hooks/use-vega-vesting";
 import { BigNumber } from "../../../lib/bignumber";
+import { Routes } from "../../router-config";
 import { RedemptionState } from "../redemption-reducer";
 import { TrancheTable } from "../tranche-table";
 
@@ -25,7 +27,13 @@ export const RedeemFromTranche = ({
   const vesting = useVegaVesting();
   const { t } = useTranslation();
   const {
-    appState: { lien, totalVestedBalance, trancheBalances, totalLockedBalance },
+    appState: {
+      lien,
+      totalVestedBalance,
+      trancheBalances,
+      totalLockedBalance,
+      contractAddresses,
+    },
   } = useAppState();
   const refreshBalances = useRefreshBalances(address);
   const getUserTrancheBalances = useGetUserTrancheBalances(address);
@@ -44,6 +52,13 @@ export const RedeemFromTranche = ({
     () => vesting.withdrawFromTranche(address, numberId),
     () => vesting.checkWithdrawFromTranche(address, numberId)
   );
+  const redeemedAmount = React.useMemo(() => {
+    return trancheBalances.find(
+      ({ id: bId }) => bId.toString() === id.toString()
+    )!.vested;
+    // TODO needs some explination
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   // If the claim has been committed refetch the new VEGA balance
   React.useEffect(() => {
     if (txState.txState === TxState.Complete && address) {
@@ -63,11 +78,34 @@ export const RedeemFromTranche = ({
       </section>
     );
   }
-
+  // TODO needs some translations
   return (
     <section className="redemption-tranche" data-testid="redemption-tranche">
       {txState.txState !== TxState.Default ? (
         <TransactionCallout
+          completeHeading={
+            <strong style={{ color: Colors.WHITE }}>
+              Tokens from this Tranche have been redeemed
+            </strong>
+          }
+          completeFooter={
+            <>
+              <p>
+                You have redeemed {redeemedAmount.toString()} VEGA tokens from
+                this tranche. They are now free to transfer from your ethereum
+                wallet.
+              </p>
+              <p>
+                The VEGA token address is {contractAddresses.vegaTokenAddress},
+                make sure you add this to your wallet to see your tokens
+              </p>
+              <p>
+                Go to <Link to={Routes.STAKING}>staking</Link> or{" "}
+                <Link to={Routes.GOVERNANCE}>governance</Link> to see how you
+                can use your unlocked tokens
+              </p>
+            </>
+          }
           state={txState}
           reset={() => txDispatch({ type: TransactionActionType.TX_RESET })}
         />
