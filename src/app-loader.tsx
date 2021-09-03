73764133ce8bcd9df9d1919735836ed1ec0ff3f7
Matthew Russell <mattrussell36@gmail.com>
2021-09-03 12:08:23 -0700

    move initial keys fetch to app loader to avoid multiple requests

--- src/app-loader.tsx
+++ src/app-loader.tsx
@@ -6,15 +6,20 @@ import {
   AppStateActionType,
   useAppState,
 } from "./contexts/app-state/app-state-context";
+import { Errors as VegaWalletServiceErrors } from "./lib/vega-wallet/vega-wallet-service";
 import { useVegaStaking } from "./hooks/use-vega-staking";
 import { useVegaToken } from "./hooks/use-vega-token";
 import { useVegaVesting } from "./hooks/use-vega-vesting";
+import { useVegaWallet } from "./hooks/use-vega-wallet";
+
+let keysFetched = false;
 
 export const AppLoader = ({ children }: { children: React.ReactElement }) => {
-  const { appDispatch } = useAppState();
+  const { appState, appDispatch } = useAppState();
   const token = useVegaToken();
   const staking = useVegaStaking();
   const vesting = useVegaVesting();
+  const vegaWalletService = useVegaWallet();
   const [loaded, setLoaded] = React.useState(false);
 
   React.useEffect(() => {
@@ -45,6 +50,42 @@ export const AppLoader = ({ children }: { children: React.ReactElement }) => {
 
     run();
   }, [token, appDispatch, staking, vesting]);
+
+  // Attempte to get vega keys on startup
+  React.useEffect(() => {
+    async function run() {
+      const [err, keys] = await vegaWalletService.getKeys();
+      keysFetched = true;
+
+      if (err === VegaWalletServiceErrors.NO_TOKEN) {
+        // do nothing, make user authenticate again
+        return;
+      }
+
+      if (err === VegaWalletServiceErrors.SERVICE_UNAVAILABLE) {
+        appDispatch({ type: AppStateActionType.VEGA_WALLET_DOWN });
+        return;
+      }
+
+      let vegaAssociatedBalance = null;
+      if (appState.address && keys && keys.length) {
+        vegaAssociatedBalance = await staking.stakeBalance(
+          appState.address,
+          keys[0].pub
+        );
+      }
+
+      appDispatch({
+        type: AppStateActionType.VEGA_WALLET_INIT,
+        keys,
+        vegaAssociatedBalance,
+      });
+    }
+
+    if (!keysFetched) {
+      run();
+    }
+  }, [appDispatch, appState.address, staking, vegaWalletService]);
 
   if (!loaded) {
     return (
