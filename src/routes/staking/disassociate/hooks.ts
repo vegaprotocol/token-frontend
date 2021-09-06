20752f07315e76e0d57ed488438a3e0ba8c6ef38
Dexter <dexter.edwards93@gmail.com>
2021-09-06 11:51:38 +0100

    kill consoole.logs

--- src/routes/staking/disassociate/hooks.ts
+++ src/routes/staking/disassociate/hooks.ts
@@ -39,10 +39,8 @@ export const useRemoveStake = (
 
   return React.useMemo(() => {
     if (stakingMethod === StakingMethod.Contract) {
-      console.log("C");
       return contractRemove;
     } else {
-      console.log("w");
       return walletRemove;
     }
   }, [contractRemove, stakingMethod, walletRemove]);
