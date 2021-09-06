fb0dfd19c16b8bdc070c9f68a4a51a33a8abc109
Dexter <dexter.edwards93@gmail.com>
2021-09-06 15:42:23 +0100

    remove console.log

--- src/lib/format-number.ts
+++ src/lib/format-number.ts
@@ -2,11 +2,5 @@ import { BigNumber } from "./bignumber";
 
 export const formatNumber = (value: BigNumber) => {
   const decimalPlaces = Math.max(value.dp(), 2);
-  console.log(
-    new BigNumber(100).dp(2).toString(),
-    decimalPlaces,
-    value.toString(),
-    value.dp(decimalPlaces).toString()
-  );
   return value.dp(decimalPlaces).toFormat(decimalPlaces);
 };
