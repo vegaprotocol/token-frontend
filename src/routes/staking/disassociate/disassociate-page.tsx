9e15cffdb0bf9d78d111d056e4e65fdc8f9eaa04
Dexter <dexter.edwards93@gmail.com>
2021-09-06 10:08:49 +0100

    correct maximum balance in disassociate page

--- src/routes/staking/disassociate/disassociate-page.tsx
+++ src/routes/staking/disassociate/disassociate-page.tsx
@@ -38,7 +38,6 @@ export const DisassociatePage = ({
   const [selectedStakingMethod, setSelectedStakingMethod] = React.useState<
     StakingMethod | ""
   >(params.method as StakingMethod | "");
-  console.log(selectedStakingMethod);
   const {
     state: txState,
     dispatch: txDispatch,
