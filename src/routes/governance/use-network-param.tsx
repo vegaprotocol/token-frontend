78dd8401d5d0f91fe020aa792b2b447e1f974536
Pablo Griscti <2323dragon@gmail.com>
2021-09-07 14:16:41 +0100

    co-locate query

--- src/routes/governance/use-network-param.tsx
+++ src/routes/governance/use-network-param.tsx
@@ -1,7 +1,16 @@
 import { useQuery } from "@apollo/client";
-import { networkParamsQuery } from "./network-params";
+import { gql } from "@apollo/client";
 
 import type { networkParams } from "./__generated__/networkParams";
+
+export const networkParamsQuery = gql`
+  query NETWORK_PARAMS_QUERY {
+    NetworkParameters {
+      key
+      value
+    }
+  }
+`;
 
 export function useNetworkParam(params: string[]) {
   const { data, loading, error } = useQuery<networkParams, never>(
