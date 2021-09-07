a1ddeae36607f618acee29c2b86be627917cb143
Dexter <dexter.edwards93@gmail.com>
2021-09-07 14:02:14 +0100

    update addresses

--- src/config/ethereum.ts
+++ src/config/ethereum.ts
@@ -32,10 +32,10 @@ export const Addresses = {
   },
   [EthereumChainIds.Ropsten]: {
     vestingAddress: "0xfc9Ad8fE9E0b168999Ee7547797BC39D55d607AA",
-    vegaTokenAddress: "0xFa521aDcc11925266A518CdE33386FcD9cF2A4A6",
+    vegaTokenAddress: "0x5b634a05754283b6d9d7938dcca9d646425593eb",
     claimAddress: "0x695eD7f6AcA81201d1D92107f120579CaAe2E5F2",
     lockedAddress: "0x0356782bfb61cf0b0463746bc6fe8766aacae8f0",
-    stakingBridge: "0x1B57E5393d949242a9AD6E029E2f8A684BFbBC08",
+    stakingBridge: "0x7bd4a4789394fe5a93fc67ef64c47beb013e5450",
   },
 };
 
