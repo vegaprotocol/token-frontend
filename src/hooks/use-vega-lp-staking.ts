5238aeffe10c8d7d5ec70d07135e5f577b6c9625
Dexter <dexter.edwards93@gmail.com>
2021-09-09 10:52:27 +0100

    more tidy

--- src/hooks/use-vega-lp-staking.ts
+++ src/hooks/use-vega-lp-staking.ts
@@ -1,18 +1,15 @@
 import React from "react";
-import Web3 from "web3";
-import { useAppState } from "../contexts/app-state/app-state-context";
 import { IVegaLPStaking } from "../lib/web3-utils";
 // @ts-ignore
 import LPStakingAbi from "../lib/VEGA_WEB3/vega-lp-staking";
+import { useWeb3 } from "./use-web3";
 
 /**
  * I think this is actually going to need to export 1x ABI per bridge, i.e. around 4
  */
 export const useVegaLPStaking = ({ address }: { address: string }) => {
-  const { provider } = useAppState();
-
+  const web3 = useWeb3();
   return React.useMemo<IVegaLPStaking>(() => {
-    const web3 = new Web3(provider);
     return new LPStakingAbi(web3, address);
-  }, [provider, address]);
+  }, [web3, address]);
 };
