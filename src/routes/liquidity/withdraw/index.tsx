9936e200caf049d9fd7cdb8a4e7dc953e9ee4bdc
Dexter <dexter.edwards93@gmail.com>
2021-09-09 17:10:17 +0100

    build out withdraw page

--- src/routes/liquidity/withdraw/index.tsx
+++ src/routes/liquidity/withdraw/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { useEthUser } from "../../../hooks/use-eth-user";
 import { useVegaLPStaking } from "../../../hooks/use-vega-lp-staking";
-import { BigNumber } from "../../../lib/bignumber";
 import { useParams } from "react-router";
 import { REWARDS_ADDRESSES } from "../../../config";
-import { TokenInput } from "../../../components/token-input";
 import { useTranslation } from "react-i18next";
+import { useTransaction } from "../../../hooks/use-transaction";
+import {
+  TransactionActionType,
+  TxState,
+} from "../../../hooks/transaction-reducer";
+import { TransactionCallout } from "../../../components/transaction-callout";
+import * as Sentry from "@sentry/react";
+import BigNumber from "bignumber.js";
 
 export const LiquidityWithdrawPage = ({
   lpTokenAddress,
@@ -13,18 +19,51 @@ export const LiquidityWithdrawPage = ({
   lpTokenAddress: string;
 }) => {
   const { t } = useTranslation();
-  const [amount, setAmount] = React.useState("0");
   const lpStaking = useVegaLPStaking({ address: lpTokenAddress });
   const { ethAddress } = useEthUser();
+  const {
+    state: txUnstakeState,
+    dispatch: txUnstakeDispatch,
+    perform: txUnstakePerform,
+  } = useTransaction(() => lpStaking.unstake(ethAddress));
+  const [unstakedBalance, setUnstakedBalance] = React.useState("0");
+  const transactionInProgress = React.useMemo(
+    () =>
+      txUnstakeState.txState !== TxState.Default &&
+      txUnstakeState.txState !== TxState.Complete,
+    [txUnstakeState.txState]
+  );
+  React.useEffect(() => {
+    const run = async () => {
+      try {
+        const stakedBalance = await lpStaking.stakedBalance(ethAddress);
+        setUnstakedBalance(stakedBalance);
+      } catch (err) {
+        Sentry.captureException(err);
+      }
+    };
+
+    run();
+  }, [lpStaking, ethAddress]);
+
+  if (new BigNumber(unstakedBalance).isEqualTo(0)) {
+    return <section>You have no LP tokens staked</section>;
+  }
+
   return (
     <section>
-      <TokenInput
-        submitText={t("Withdraw {{address}}", { address: lpTokenAddress })}
-        perform={() => undefined}
-        amount={amount}
-        setAmount={setAmount}
-        maximum={new BigNumber(100)}
-      />
+      {transactionInProgress ? (
+        <TransactionCallout
+          state={txUnstakeState}
+          reset={() =>
+            txUnstakeDispatch({ type: TransactionActionType.TX_RESET })
+          }
+        />
+      ) : (
+        <button className="fill" onClick={txUnstakePerform}>
+          Withdraw all tokens
+        </button>
+      )}
     </section>
   );
 };
