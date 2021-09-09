2dee6856adc719345d7721c4af27c5ad8a8086bc
Dexter <dexter.edwards93@gmail.com>
2021-09-09 10:51:01 +0100

    tidy some duplicated kogic

--- src/hooks/use-vega-staking.ts
+++ src/hooks/use-vega-staking.ts
@@ -1,19 +1,17 @@
 import React from "react";
-import Web3 from "web3";
 import { useAppState } from "../contexts/app-state/app-state-context";
 import { IVegaStaking } from "../lib/web3-utils";
 // @ts-ignore
 import StakingAbi from "../lib/VEGA_WEB3/vega-staking";
 import { ADDRESSES } from "../config";
+import { useWeb3 } from "./use-web3";
 
 export const useVegaStaking = () => {
   const {
-    provider,
     appState: { decimals },
   } = useAppState();
-
+  const web3 = useWeb3();
   return React.useMemo<IVegaStaking>(() => {
-    const web3 = new Web3(provider);
     return new StakingAbi(web3, ADDRESSES.stakingBridge, decimals);
-  }, [provider, decimals]);
+  }, [web3, decimals]);
 };
--- src/hooks/use-vega-token.ts
+++ src/hooks/use-vega-token.ts
@@ -1,16 +1,13 @@
 import React from "react";
-import Web3 from "web3";
 import { ADDRESSES } from "../config";
-import { useAppState } from "../contexts/app-state/app-state-context";
 // @ts-ignore VEGA_WEB3 path swapped depending on prod build or test build
 import VegaToken from "../lib/VEGA_WEB3/vega-token";
 import { IVegaToken } from "../lib/web3-utils";
+import { useWeb3 } from "./use-web3";
 
 export function useVegaToken() {
-  const { provider } = useAppState();
-
+  const web3 = useWeb3();
   return React.useMemo<IVegaToken>(() => {
-    const web3 = new Web3(provider);
     return new VegaToken(web3, ADDRESSES.vegaTokenAddress);
-  }, [provider]);
+  }, [web3]);
 }
