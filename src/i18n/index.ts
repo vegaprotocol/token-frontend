70ea69e9738478f1ab725b704a499cd78eaca27d
Dexter <dexter.edwards93@gmail.com>
2021-09-16 13:56:19 +0100

    use json files for translations

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import english from "./en";
+import english from "./en.json";
 
 const en = {
   translations: english,
