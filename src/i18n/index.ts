2d7b7d7b76c160253e30a8f0f40806dcb061e0d7
Dexter <dexter.edwards93@gmail.com>
2021-09-17 16:24:25 +0100

    2 letter codes fixes

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,17 +3,17 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import { Flags } from "../config";
 import dev from "./translations/dev.json";
-import en from "./translations/en.json";
-import fr from "./translations/fr.json";
-import ru from "./translations/ru.json";
-import zh from "./translations/zh.json";
-import zu from "./translations/zu.json";
+import en from "./translations/en-US.json";
+import fr from "./translations/fr-FR.json";
+import ru from "./translations/ru-RU.json";
+import zh from "./translations/zh-CN.json";
+import zu from "./translations/zu-ZA.json";
 
 const isInContextTranslation = Flags.IN_CONTEXT_TRANSLATION;
 
 const psuedoLanguage = {
   keys: zu,
-  locale: "zu",
+  locale: "zu-ZA",
 };
 
 i18n
