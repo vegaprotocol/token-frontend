9e56767ed546c7a0e0831ab54423dc72397b239a
Matthew Russell <mattrussell36@gmail.com>
2021-09-30 16:25:51 +0100

    add environment property to sentry init args

--- src/index.tsx
+++ src/index.tsx
@@ -16,6 +16,7 @@ if (dsn) {
     dsn,
     integrations: [new Integrations.BrowserTracing()],
     tracesSampleRate: 0.1,
+    environment: process.env.REACT_APP_ENV,
     beforeSend(event) {
       if (event.request?.url?.includes("/claim?")) {
         return {
