c2a1654bf21026dfe94cc5cb67966ac8caa01502
Pablo Griscti <2323dragon@gmail.com>
2021-10-04 12:20:14 +0100

    format ternary operator

--- src/routes/staking/disassociate/disassociate-page-container.tsx
+++ src/routes/staking/disassociate/disassociate-page-container.tsx
@@ -1,17 +1,16 @@
 import "./disassociate-page.scss";
 
-import { StakingWalletsContainer } from "../staking-wallets-container";
 import { DisassociatePage } from "./disassociate-page";
 import { DisassociatePageNoVega } from "./disassociate-page-no-vega";
+import { StakingWalletsContainer } from "../staking-wallets-container";
 
 export const DisassociateContainer = () => {
   return (
     <StakingWalletsContainer needsEthereum={true} needsVega={false}>
       {({ address, currVegaKey = null }) => (
-        currVegaKey ?
-          <DisassociatePage address={address} vegaKey={currVegaKey} />
-          :
-          <DisassociatePageNoVega />
+        currVegaKey
+        ? <DisassociatePage address={address} vegaKey={currVegaKey} />
+        : <DisassociatePageNoVega />
       )}
     </StakingWalletsContainer>
   );
