485ff24ad582f25128c8ea21af1ee1cd6e546a83
Matthew Russell <mattrussell36@gmail.com>
2021-10-05 14:58:49 -0700

    add hook to poll for the available stake linking once we have a txhash

--- src/routes/staking/associate/associate-page.tsx
+++ src/routes/staking/associate/associate-page.tsx
@@ -15,6 +15,11 @@ import {
 } from "../../../components/staking-method-radio";
 import { useAddStake } from "./hooks";
 import { BigNumber } from "../../../lib/bignumber";
+import { useQuery, gql } from "@apollo/client";
+import {
+  PartyStakeLinkings,
+  PartyStakeLinkingsVariables,
+} from "./__generated__/PartyStakeLinkings";
 
 export const AssociatePage = ({
   address,
@@ -48,6 +53,8 @@ export const AssociatePage = ({
     requiredConfirmations
   );
 
+  const linking = usePollForStakeLinking(vegaKey.pub, txState.txData.hash);
+
   const {
     appState: { walletBalance, totalVestedBalance },
   } = useAppState();
@@ -66,6 +73,7 @@ export const AssociatePage = ({
       />
     );
   }
+
   return (
     <section data-testid="associate">
       <p data-testid="associate-information">
@@ -106,3 +114,53 @@ export const AssociatePage = ({
     </section>
   );
 };
+
+const PARTY_STAKE_LINKINGS = gql`
+  query PartyStakeLinkings($partyId: ID!) {
+    party(id: $partyId) {
+      stake {
+        linkings {
+          id
+          txHash
+        }
+      }
+    }
+  }
+`;
+
+function usePollForStakeLinking(partyId: string, txHash: string | null) {
+  // Query for linkings under current connected party (vega key)
+  const { data, startPolling, stopPolling } = useQuery<
+    PartyStakeLinkings,
+    PartyStakeLinkingsVariables
+  >(PARTY_STAKE_LINKINGS, {
+    variables: { partyId },
+  });
+
+  // Everytime we get data, find the matching linking by txHash
+  const linking = React.useMemo(() => {
+    const linkings = data?.party?.stake.linkings;
+
+    if (linkings?.length) return;
+
+    const matchingLinking = linkings?.find((l) => l.txHash === txHash);
+
+    return matchingLinking;
+  }, [data, txHash]);
+
+  // Start and stop polling if we have a linking/txHash or if unmount
+  React.useEffect(() => {
+    // If we have the txHash of the transaction but no found linking start polling
+    if (!linking && txHash) {
+      startPolling(1000);
+    } else {
+      stopPolling();
+    }
+
+    return () => {
+      stopPolling();
+    };
+  }, [startPolling, stopPolling, linking, txHash]);
+
+  return linking;
+}
