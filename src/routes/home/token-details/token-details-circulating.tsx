ef2f175a8fabc6baa66e400c92500fe6da8cdf8a
Dexter <dexter.edwards93@gmail.com>
2021-10-11 13:43:23 +0100

    remove redeemed tokens

--- src/routes/home/token-details/token-details-circulating.tsx
+++ src/routes/home/token-details/token-details-circulating.tsx
@@ -3,11 +3,11 @@ import { formatNumber } from "../../../lib/format-number";
 import { Tranche } from "../../../lib/vega-web3/vega-web3-types";
 
 /**
- * Add together the reedeemed tokens from all tranches
+ * Add together the circulating tokens from all tranches
  *
  * @param tranches All of the tranches to sum
  * @param decimals decimal places for the formatted result
- * @return Total redeemed vouchers, formatted as a string
+ * @return The total circulating tokens from all tranches
  */
 export function sumCirculatingTokens(tranches: Tranche[] | null): BigNumber {
   let totalCirculating: BigNumber = new BigNumber(0);
@@ -40,35 +40,5 @@ export const TokenDetailsCirculating = ({
     <td data-testid="circulating-supply">
       {formatNumber(totalCirculating, 2)}
     </td>
-  );
-};
-
-export function sumRedeemedTokens(tranches: Tranche[] | null): BigNumber {
-  let totalCirculating: BigNumber = new BigNumber(0);
-
-  tranches?.forEach(
-    (tranche) =>
-      (totalCirculating = totalCirculating.plus(tranche.total_removed))
-  );
-
-  return totalCirculating;
-}
-
-/**
- * Renders a table cell containing the total circulating number of Vega tokens, which is the
- * sum of all redeemed tokens across all tranches
- *
- * @param tranches An array of all of the tranches
- * @param decimals Decimal places for this token
- * @constructor
- */
-export const TokenDetailsTotalRedeemed = ({
-  tranches,
-}: {
-  tranches: Tranche[] | null;
-}) => {
-  const totalCirculating = sumRedeemedTokens(tranches);
-  return (
-    <td data-testid="redeemed-supply">{formatNumber(totalCirculating, 2)}</td>
   );
 };
