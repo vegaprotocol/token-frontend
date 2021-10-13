b1f99c33301b554fff2220b09f3683918cdea57a
Dexter <dexter.edwards93@gmail.com>
2021-10-13 15:19:56 +0100

    build dumb table

---
+++ src/routes/rewards/home/__generated__/Rewards.ts
@@ -0,0 +1,83 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: Rewards
+// ====================================================
+
+export interface Rewards_party_rewardDetails_asset {
+  __typename: "Asset";
+  /**
+   * The id of the asset
+   */
+  id: string;
+  /**
+   * The symbol of the asset (e.g: GBP)
+   */
+  symbol: string;
+}
+
+export interface Rewards_party_rewardDetails_rewards {
+  __typename: "Reward";
+  /**
+   * The asset for which this reward is associated
+   */
+  assetId: string;
+  /**
+   * Party receiving the reward
+   */
+  partyId: string;
+  /**
+   * Epoch for which this reward was distributed
+   */
+  epoch: number;
+  /**
+   * Amount received for this reward
+   */
+  amount: string;
+  /**
+   * Percentage out of the total distributed reward
+   */
+  percentageOfTotal: string;
+  /**
+   * Time at which the rewards was received
+   */
+  receivedAt: string;
+}
+
+export interface Rewards_party_rewardDetails {
+  __typename: "RewardPerAssetDetail";
+  /**
+   * Asset in which the reward was paid
+   */
+  asset: Rewards_party_rewardDetails_asset;
+  /**
+   * A list of rewards received for this asset
+   */
+  rewards: (Rewards_party_rewardDetails_rewards | null)[] | null;
+  /**
+   * The total amount of rewards received for this asset.
+   */
+  totalAmount: string;
+}
+
+export interface Rewards_party {
+  __typename: "Party";
+  /**
+   * return reward information
+   */
+  rewardDetails: (Rewards_party_rewardDetails | null)[] | null;
+}
+
+export interface Rewards {
+  /**
+   * An entity that is trading on the VEGA network
+   */
+  party: Rewards_party | null;
+}
+
+export interface RewardsVariables {
+  partyId: string;
+}
--- src/routes/rewards/home/index.tsx
+++ src/routes/rewards/home/index.tsx
@@ -4,8 +4,11 @@ import gql from "graphql-tag";
 import { Trans, useTranslation } from "react-i18next";
 import { SplashLoader } from "../../../components/splash-loader";
 import { SplashScreen } from "../../../components/splash-screen";
-import { EpochCountdown } from "../../staking/epoch-countdown";
 import { Epoch } from "./__generated__/Epoch";
+import { EpochCountdown } from "../../../components/epoch-countdown";
+import { Rewards } from "./__generated__/Rewards";
+import { useVegaUser } from "../../../hooks/use-vega-user";
+import { Colors } from "../../../config";
 
 export const EPOCH_QUERY = gql`
   query Epoch {
@@ -14,6 +17,29 @@ export const EPOCH_QUERY = gql`
       timestamps {
         start
         end
+        expiry
+      }
+    }
+  }
+`;
+
+export const REWARDS_QUERY = gql`
+  query Rewards($partyId: ID!) {
+    party(id: $partyId) {
+      rewardDetails {
+        asset {
+          id
+          symbol
+        }
+        rewards {
+          assetId
+          partyId
+          epoch
+          amount
+          percentageOfTotal
+          receivedAt
+        }
+        totalAmount
       }
     }
   }
@@ -21,15 +47,24 @@ export const EPOCH_QUERY = gql`
 
 export const RewardsIndex = () => {
   const { t } = useTranslation();
+  const { currVegaKey } = useVegaUser();
   const { data, loading, error } = useQuery<Epoch>(EPOCH_QUERY);
-  if (loading || !data?.epoch) {
+  const {
+    data: rewardsData,
+    loading: rewardsLoading,
+    error: rewardsError,
+  } = useQuery<Rewards>(REWARDS_QUERY, {
+    variables: { partyId: currVegaKey?.pub },
+    skip: !currVegaKey?.pub,
+  });
+  if (loading || !data?.epoch || rewardsLoading) {
     return (
       <SplashScreen>
         <SplashLoader />
       </SplashScreen>
     );
   }
-  console.log(data, loading, error);
+  console.log(rewardsData, data!.epoch);
   return (
     <section className="rewards">
       <p>
@@ -52,25 +87,30 @@ export const RewardsIndex = () => {
       {!loading &&
         !error &&
         data.epoch.timestamps.start &&
-        data.epoch.timestamps.end && (
+        data.epoch.timestamps.expiry && (
           <EpochCountdown
             containerClass="staking-node__epoch"
             id={data!.epoch.id}
             startDate={new Date(data.epoch.timestamps.start)}
-            endDate={new Date(data.epoch.timestamps.end)}
+            endDate={new Date(data.epoch.timestamps.expiry!)}
           />
         )}
       <h3>Recent epochs</h3>
-      <table style={{ textAlign: "left", width: "100%" }}>
+      <p>
+        Total rewards:{" "}
+        {rewardsData?.party?.rewardDetails &&
+          rewardsData.party.rewardDetails[0]?.totalAmount}
+      </p>
+      <table style={{ textAlign: "left", width: "100%", color: Colors.WHITE }}>
         <thead>
           <tr>
             <th>
               <div>Epoch number</div>
-              <div>Epoch end</div>
+              <div className="text-deemphasise">Epoch end</div>
             </th>
             <th>You score based share %</th>
             <th>
-              <div>Total (Fees+Reward)</div>
+              <div>Total (Fees + Reward)</div>
               <select>
                 <option>VEGA tokens</option>
               </select>
@@ -78,118 +118,19 @@ export const RewardsIndex = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>-</td>
-          </tr>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>1,000.00</td>
-          </tr>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>1,000.00</td>
-          </tr>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>1,000.00</td>
-          </tr>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>1,000.00</td>
-          </tr>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>1,000.00</td>
-          </tr>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>1,000.00</td>
-          </tr>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>1,000.00</td>
-          </tr>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>1,000.00</td>
-          </tr>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>1,000.00</td>
-          </tr>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>1,000.00</td>
-          </tr>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>1,000.00</td>
-          </tr>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>1,000.00</td>
-          </tr>
-          <tr>
-            <td>
-              <div>14 - Current</div>
-              <div>17 Sept 2021 23:59</div>
-            </td>
-            <td style={{ textAlign: "right" }}>0.09</td>
-            <td style={{ textAlign: "right" }}>1,000.00</td>
-          </tr>
+          {/* TODO find reward selected from asset list */}
+          {rewardsData?.party?.rewardDetails &&
+            rewardsData?.party?.rewardDetails[0]?.rewards &&
+            rewardsData.party.rewardDetails[0]?.rewards.map((r) => (
+              <tr>
+                <td>
+                  <div>{r?.epoch}</div>
+                  <div className="text-deemphasise">17 Sept 2021 23:59</div>
+                </td>
+                <td style={{ textAlign: "right" }}>{r?.percentageOfTotal}</td>
+                <td style={{ textAlign: "right" }}>{r?.amount}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </section>
