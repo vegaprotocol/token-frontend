40728d78dec7b58e5b6b740a286f330643b7da05
Matthew Russell <mattrussell36@gmail.com>
2021-10-13 15:44:01 -0700

    remove test console log

--- src/contexts/web3-context/web3-provider.tsx
+++ src/contexts/web3-context/web3-provider.tsx
@@ -9,8 +9,6 @@ import Web3 from "web3";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
-console.log(process.env.REACT_APP_INFURA_ID);
-
 const web3Modal = new Web3Modal({
   cacheProvider: true,
   theme: "dark",
