d4fb7338bc97d6552f689cb6fe39c9616816abc0
Matthew Russell <mattrussell36@gmail.com>
2021-10-13 17:56:51 -0700

    adjust font sizes of heading area making breadcrums smaller and nav links bigger, also truncate staking node page

--- src/components/nav/breadcrumbs.tsx
+++ src/components/nav/breadcrumbs.tsx
@@ -1,23 +1,27 @@
 import "./breadcrumbs.scss";
 
-import { useHistory, useRouteMatch } from "react-router-dom";
+import { Link, useRouteMatch } from "react-router-dom";
 import useBreadcrumbs from "use-react-router-breadcrumbs";
+import { truncateMiddle } from "../../lib/truncate-middle";
+import React from "react";
 
 export const Breadcrumbs = () => {
   const breadcrumbs = useBreadcrumbs();
-  const history = useHistory();
   const isClaim = useRouteMatch("/claim");
+  console.log(breadcrumbs);
   return !isClaim && breadcrumbs.length > 1 ? (
     <div className="breadcrumbs">
       {breadcrumbs.map(({ breadcrumb, match }, index) => (
         <div key={`${index}-container`} data-testid="breadcrumb">
-          <button
-            className="button-link"
-            key={`${index}-link`}
-            onClick={() => history.push(match.url)}
-          >
-            {breadcrumb}
-          </button>
+          <Link to={match.url} key={`${index}-link`}>
+            {isStakeNodePage(match.url)
+              ? React.cloneElement(breadcrumb as React.ReactElement, {
+                  children: truncateMiddle(
+                    (breadcrumb as React.ReactElement).props.children
+                  ),
+                })
+              : breadcrumb}
+          </Link>
           <span key={`${index}-spacer`} className="breadcrumbs__spacer">
             {index !== breadcrumbs.length - 1 && ">"}
           </span>
@@ -26,3 +30,7 @@ export const Breadcrumbs = () => {
     </div>
   ) : null;
 };
+
+function isStakeNodePage(url: string) {
+  return /\/staking\/[a-zA-Z0-9]+$/.test(url);
+}
