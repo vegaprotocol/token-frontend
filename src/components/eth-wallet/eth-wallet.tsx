054b2bf3f3e61e12933966d13213d7b451f9d059
Matthew Russell <mattrussell36@gmail.com>
2021-10-21 20:38:21 -0700

    add title row to indicate associations by vega key

--- src/components/eth-wallet/eth-wallet.tsx
+++ src/components/eth-wallet/eth-wallet.tsx
@@ -122,16 +122,13 @@ const ConnectedKey = () => {
       {Flags.STAKING_DISABLED || Flags.REDEEM_DISABLED ? null : (
         <>
           <hr style={{ borderStyle: "dashed", color: Colors.TEXT }} />
-          {/* <WalletCardRow
-            label={t("Associated")}
-            value={lien}
-            valueSuffix={t("VEGA")}
-          /> */}
+          <WalletCardRow label="Associated by Vega key" dark={true} />
           {Object.entries(associations).map(([key, amount]) => (
             <WalletCardRow
               key={key}
               label={truncateMiddle(key)}
               value={amount}
+              valueSuffix={t("VEGA")}
             />
           ))}
         </>
