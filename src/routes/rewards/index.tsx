c5c3eb0e93555b9c3a182f1f5a00b60380aad854
Matthew Russell <mattrussell36@gmail.com>
2021-11-05 14:33:13 -0700

    removed template sidebard from rewards page

--- src/routes/rewards/index.tsx
+++ src/routes/rewards/index.tsx
@@ -1,24 +1,19 @@
-import { useTranslation } from "react-i18next";
 import { Route, Switch, useRouteMatch } from "react-router";
 import { RouteChildProps } from "..";
-import { TemplateDefault } from "../../components/page-templates/template-default";
 import { useDocumentTitle } from "../../hooks/use-document-title";
 import { RewardsIndex } from "./home";
 
 const RewardRouter = ({ name }: RouteChildProps) => {
-  const { t } = useTranslation();
   useDocumentTitle(name);
   const match = useRouteMatch();
 
   return (
-    <TemplateDefault title={t("pageTitleRewards")}>
-      <Switch>
-        <Route path={match.path} exact>
-          <RewardsIndex />
-        </Route>
-        <Route path={`${match.path}/withdraw`}></Route>
-      </Switch>
-    </TemplateDefault>
+    <Switch>
+      <Route path={match.path} exact>
+        <RewardsIndex />
+      </Route>
+      <Route path={`${match.path}/withdraw`}></Route>
+    </Switch>
   );
 };
 
