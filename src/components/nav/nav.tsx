2820543075952283fadc1ac90cc6428d3cb1e5a1
Matthew Russell <mattrussell36@gmail.com>
2021-11-09 12:37:24 -0800

    add rewards link to top nav

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -205,6 +205,9 @@ const NavLinks = ({
       <NavLink {...linkProps} to={Routes.STAKING}>
         {t("Staking")}
       </NavLink>
+      <NavLink {...linkProps} to={Routes.REWARDS}>
+        {t("Rewards")}
+      </NavLink>
       {Flags.GOVERNANCE_DISABLED ? null : (
         <NavLink {...linkProps} to={Routes.GOVERNANCE}>
           {t("Governance")}
