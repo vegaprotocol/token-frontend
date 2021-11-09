49c6a20acbe775bfb1abb1cdbe32f94bfd155a29
Matthew Russell <mattrussell36@gmail.com>
2021-11-09 13:51:16 -0800

    add RewardTable component so that stake per epoch is more easily memoised

--- src/routes/rewards/home/reward-info.tsx
+++ src/routes/rewards/home/reward-info.tsx
@@ -4,13 +4,21 @@ import {
   KeyValueTable,
   KeyValueTableRow,
 } from "../../../components/key-value-table";
-import { Rewards } from "./__generated__/Rewards";
+import {
+  Rewards,
+  Rewards_party_delegations,
+  Rewards_party_rewardDetails_rewards,
+} from "./__generated__/Rewards";
 import { VegaKeyExtended } from "../../../contexts/app-state/app-state-context";
 
 interface RewardInfoProps {
   data: Rewards | undefined;
   currVegaKey: VegaKeyExtended;
 }
+
+// Note: For now the only reward type is Staking. We'll need this from the API
+// at a later date
+const DEFAULT_REWARD_TYPE = "Staking";
 
 export const RewardInfo = ({ data, currVegaKey }: RewardInfoProps) => {
   // Create array of rewards per epoch
@@ -49,48 +57,65 @@ export const RewardInfo = ({ data, currVegaKey }: RewardInfoProps) => {
     <>
       <h2>Rewards for {currVegaKey?.pubShort}</h2>
       {vegaTokenRewards.map((reward, i) => {
-        console.log(reward);
         if (!reward) return null;
-
-        let stakeForEpoch = "0";
-
-        if (data?.party?.delegations?.length) {
-          const delegationForEpoch = data.party.delegations.find(
-            (d) => d.epoch === Number(reward.epoch.id)
-          );
-          if (delegationForEpoch) {
-            stakeForEpoch = delegationForEpoch.amountFormatted;
-          }
-        }
-
         return (
-          <div key={i}>
-            <h3>Epoch {reward.epoch.id}</h3>
-            <KeyValueTable>
-              <KeyValueTableRow>
-                <th>Reward type</th>
-                <td>Staking</td>
-              </KeyValueTableRow>
-              <KeyValueTableRow>
-                <th>Your stake</th>
-                <td>{stakeForEpoch.toString()}</td>
-              </KeyValueTableRow>
-              <KeyValueTableRow>
-                <th>Reward</th>
-                <td>{reward.amountFormatted} VEGA</td>
-              </KeyValueTableRow>
-              <KeyValueTableRow>
-                <th>Share of reward</th>
-                <td>{reward.percentageOfTotal}</td>
-              </KeyValueTableRow>
-              <KeyValueTableRow>
-                <th>Received</th>
-                <td>{reward.receivedAt}</td>
-              </KeyValueTableRow>
-            </KeyValueTable>
-          </div>
+          <RewardTable
+            key={i}
+            reward={reward}
+            delegations={data?.party?.delegations || []}
+          />
         );
       })}
     </>
+  );
+};
+
+interface RewardTableProps {
+  reward: Rewards_party_rewardDetails_rewards;
+  delegations: Rewards_party_delegations[];
+}
+
+export const RewardTable = ({ reward, delegations }: RewardTableProps) => {
+  // Get your stake for epoch in which you have rewards
+  const stakeForEpoch = React.useMemo(() => {
+    if (!delegations.length) return "0";
+
+    const delegationForEpoch = delegations.find(
+      (d) => d.epoch === Number(reward.epoch.id)
+    );
+
+    if (delegationForEpoch) {
+      return delegationForEpoch.amountFormatted;
+    }
+
+    return "0";
+  }, [delegations, reward.epoch.id]);
+
+  return (
+    <div>
+      <h3>Epoch {reward.epoch.id}</h3>
+      <KeyValueTable>
+        <KeyValueTableRow>
+          <th>Reward type</th>
+          <td>{DEFAULT_REWARD_TYPE}</td>
+        </KeyValueTableRow>
+        <KeyValueTableRow>
+          <th>Your stake</th>
+          <td>{stakeForEpoch.toString()}</td>
+        </KeyValueTableRow>
+        <KeyValueTableRow>
+          <th>Reward</th>
+          <td>{reward.amountFormatted} VEGA</td>
+        </KeyValueTableRow>
+        <KeyValueTableRow>
+          <th>Share of reward</th>
+          <td>{reward.percentageOfTotal}</td>
+        </KeyValueTableRow>
+        <KeyValueTableRow>
+          <th>Received</th>
+          <td>{reward.receivedAt}</td>
+        </KeyValueTableRow>
+      </KeyValueTable>
+    </div>
   );
 };
