26921d86cd82d2f4e2af507582c2c9e75dabd24d
Matthew Russell <mattrussell36@gmail.com>
2021-11-16 15:20:38 -0800

    update erc20 bridge contract address

--- src/config/ethereum.ts
+++ src/config/ethereum.ts
@@ -75,7 +75,7 @@ export const EnvironmentConfig: { [key in Networks]: VegaContracts } = {
     claimAddress: "0x8Cef746ab7C83B61F6461cC92882bD61AB65a994", // TODO not deployed to this env, but random address so app doesn't error
     lockedAddress: "0x0", // TODO not deployed to this env
     stakingBridge: "0x7D88CD817227D599815d407D929af18Bb8D57176",
-    erc20Bridge: "0xb4E6cF254B61332BC93ffE96bD836561F7D8F37c",
+    erc20Bridge: "0xc0835e6dEf177F8ba2561C4e4216827A3798c6B9",
   },
   [Networks.TESTNET]: {
     vestingAddress: "0xe2deBB240b43EDfEBc9c38B67c0894B9A92Bf07c",
