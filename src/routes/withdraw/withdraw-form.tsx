c8c2ea2eb2357fa344a369737308bd241786c0ca
Matthew Russell <mattrussell36@gmail.com>
2021-11-18 15:01:46 -0800

    use eth wallet container

--- src/routes/withdraw/withdraw-form.tsx
+++ src/routes/withdraw/withdraw-form.tsx
@@ -16,7 +16,7 @@ import { StatefulButton } from "../../components/stateful-button";
 import { Loader } from "../../components/loader";
 import { Routes } from "../router-config";
 import { useTranslation } from "react-i18next";
-import { EthConnectPrompt } from "../../components/eth-connect-prompt";
+import { EthWalletContainer } from "../../components/eth-wallet-container";
 
 interface WithdrawFormProps {
   accounts: WithdrawPage_party_accounts[];
@@ -100,7 +100,9 @@ export const WithdrawForm = ({ accounts, currVegaKey }: WithdrawFormProps) => {
         )}
       </FormGroup>
       <FormGroup label={t("Connected Ethereum address")}>
-        {ethAddress ? <p>{ethAddress}</p> : <EthConnectPrompt />}
+        <EthWalletContainer>
+          {(ethAddress) => <p>{ethAddress}</p>}
+        </EthWalletContainer>
       </FormGroup>
       <FormGroup label={t("withdrawFormAmountLabel")} labelFor="amount">
         <AmountInput
