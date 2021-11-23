905ccd050fb8e1510fe37b7f486f12b7476945c3
Dexter <dexter.edwards93@gmail.com>
2021-11-23 17:03:03 +0000

    add footer to wallet sidebar

---
+++ src/components/vega-wallet/download-wallet-prompt.tsx
@@ -0,0 +1,21 @@
+import { useTranslation } from "react-i18next";
+import { Links } from "../../config";
+
+export const DownloadWalletPrompt = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <h3>{t("getWallet")}</h3>
+      <p style={{ margin: 0 }}>
+        <a href={Links.WALLET_GUIDE} target="_blank" rel="noreferrer">
+          {t("readGuide")}
+        </a>
+      </p>
+      <p style={{ margin: 0 }}>
+        <a href={Links.WALLET_RELEASES} target="_blank" rel="noreferrer">
+          {t("downloadWallet")}
+        </a>
+      </p>
+    </>
+  );
+};
