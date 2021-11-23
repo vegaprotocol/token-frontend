39248bbcb2f1d10232044bde750ca2b9efb217eb
Dexter <dexter.edwards93@gmail.com>
2021-11-23 16:54:49 +0000

    lint

--- src/components/vega-wallet/vega-wallet-modal.tsx
+++ src/components/vega-wallet/vega-wallet-modal.tsx
@@ -1,5 +1,5 @@
 import { Overlay } from "@blueprintjs/core";
-import { Trans, useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { Links } from "../../config";
 import {
   AppStateActionType,
