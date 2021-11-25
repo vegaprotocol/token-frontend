e4316bd1b4a815d04bdffe1b00743946d5f0b977
pgriscti <2323dragon@gmail.com>
2021-11-25 11:55:04 +0000

    fix merge

--- src/routes/governance/vote-details.tsx
+++ src/routes/governance/vote-details.tsx
@@ -1,32 +1,30 @@
 import "./vote-details.scss";
-import React from "react";
+import { formatDistanceToNow } from "date-fns";
 import { useTranslation } from "react-i18next";
-import { Proposals_proposals } from "./__generated__/Proposals";
-import { VoteButtons } from "./vote-buttons";
+import { useVoteInformation } from "./hooks";
+import { VoteProgress } from "./vote-progress";
+import { CurrentProposalStatus } from "./current-proposal-status";
+import { VoteButtonsContainer } from "./vote-buttons";
 import { useUserVote } from "./use-user-vote";
-import { gql, useQuery } from "@apollo/client";
-import { Parties } from "./__generated__/Parties";
-import { SplashScreen } from "../../components/splash-screen";
-import { SplashLoader } from "../../components/splash-loader";
-import { Callout } from "../../components/callout";
-
-export const PARTIES_QUERY = gql`
-  query Parties {
-    parties {
-      id
-      stake {
-        currentStakeAvailable
-        currentStakeAvailableFormatted @client
-      }
-    }
-  }
-`;
+import { ProposalState } from "../../__generated__/globalTypes";
+import { Proposal_proposal } from "./__generated__/Proposal";
 
 interface VoteDetailsProps {
   proposal: Proposal_proposal;
 }
 
 export const VoteDetails = ({ proposal }: VoteDetailsProps) => {
+  const {
+    totalTokensPercentage,
+    participationMet,
+    totalTokensVoted,
+    noPercentage,
+    yesPercentage,
+    yesTokens,
+    noTokens,
+    requiredMajorityPercentage,
+    requiredParticipation,
+  } = useVoteInformation({ proposal });
   const { t } = useTranslation();
   const { voteState, votePending, voteDatetime, castVote } = useUserVote(
     proposal.id,
@@ -34,35 +32,67 @@ export const VoteDetails = ({ proposal }: VoteDetailsProps) => {
     proposal.votes.no.votes
   );
 
-  const { data, loading, error } = useQuery<Parties>(PARTIES_QUERY);
+  const daysLeft = t("daysLeft", {
+    daysLeft: formatDistanceToNow(new Date(proposal.terms.closingDatetime)),
+  });
 
-  const party = React.useMemo(() => {
-    if (!data || !data.parties || data.parties?.length === 0) {
-      return null;
-    }
-
-    return data.parties.find((party) => party.id === proposal.party.id);
-  }, [data, proposal.party.id]);
-
-  if (loading) {
-    return (
-      <SplashScreen>
-        <SplashLoader />
-      </SplashScreen>
-    );
-  }
-
-  if (error) {
-    return (
-      <Callout intent="error" title={t("Something went wrong")}>
-        <p>{t("partiesQueryFailed")}</p>
-      </Callout>
-    );
-  }
   return (
     <section>
-      <VoteButtons
-        party={party}
+      <h4 className="proposal__sub-title">{t("votes")}</h4>
+      <div>
+        <p className="proposal__set_to">
+          {t("setTo")}
+          <span className="proposal-toast__success-text">
+            <CurrentProposalStatus proposal={proposal} />
+          </span>
+          .&nbsp;
+          {proposal.state === ProposalState.Open ? daysLeft : null}
+        </p>
+        <table className="proposal-toast__table">
+          <thead>
+            <tr>
+              <th>{t("for")}</th>
+              <th>
+                <VoteProgress
+                  threshold={requiredMajorityPercentage}
+                  progress={yesPercentage}
+                />
+              </th>
+              <th>{t("against")}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{yesPercentage.toFixed(2)}%</td>
+              <td className="proposal-toast__summary">
+                {t("majorityRequired")} {requiredMajorityPercentage.toFixed(2)}%
+              </td>
+              <td>{noPercentage.toFixed(2)}%</td>
+            </tr>
+            <tr>
+              <td className="proposal-toast__deemphasise">{yesTokens}</td>
+              <td></td>
+              <td className="proposal-toast__deemphasise">{noTokens}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        {t("participation")}
+        {": "}
+        {participationMet ? (
+          <span className="proposal-toast__participation-met">{t("met")}</span>
+        ) : (
+          <span className="proposal-toast__participation-not-met">
+            {t("notMet")}
+          </span>
+        )}{" "}
+        {totalTokensVoted} {totalTokensPercentage}%
+        <span className="proposal-toast__required-participation text-deemphasise">
+          ({Number(requiredParticipation) * 100}% {t("governanceRequired")})
+        </span>
+      </div>
+      <VoteButtonsContainer
         voteState={voteState}
         castVote={castVote}
         voteDatetime={voteDatetime}
