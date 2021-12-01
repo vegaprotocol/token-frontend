1ea9d7db07b083bf945a1f7c2bd2adbc4871a178
Dexter <dexter.edwards93@gmail.com>
2021-12-01 10:15:14 +0000

    change link

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,7 +1,7 @@
 export const Links = {
   WALLET_RELEASES: "https://github.com/vegaprotocol/go-wallet/releases",
   WALLET_GUIDE:
-    "https://docs.fairground.vega.xyz/docs/vega-wallet/create-wallet/",
+    "https://docs.vega.xyz/docs/tools/vega-wallet/cli-wallet/create-wallet",
   INCENTIVE_PROGRAM_BLOG_POST:
     "https://blog.vega.xyz/sushiswap-how-to-get-vega-liquidity-rewards-9848e4cadee9",
   SUSHI_PAIRS: "https://analytics.sushi.com/pairs/",
