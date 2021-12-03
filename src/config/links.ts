3d6cadb38af193cd41c9ce0c6d464ee3ea96daa4
Dexter <dexter.edwards93@gmail.com>
2021-12-03 17:39:23 +0000

    copy changes as per candida

--- src/config/links.ts
+++ src/config/links.ts
@@ -2,9 +2,8 @@ export const Links = {
   WALLET_RELEASES: "https://github.com/vegaprotocol/go-wallet/releases",
   WALLET_GUIDE:
     "https://docs.vega.xyz/docs/tools/vega-wallet/cli-wallet/create-wallet",
-  INCENTIVE_PROGRAM_BLOG_POST:
-    "https://blog.vega.xyz/sushiswap-how-to-get-vega-liquidity-rewards-9848e4cadee9",
   SUSHI_PAIRS: "https://analytics.sushi.com/pairs/",
   NOLT: "https://vega-testnet.nolt.io",
   GITHUB: "https://github.com/vegaprotocol/token-frontend",
+  DISCORD: "https://vega.xyz/discord",
 };
--- src/routes/liquidity/liquidity-container.tsx
+++ src/routes/liquidity/liquidity-container.tsx
@@ -1,50 +1,30 @@
 import "./liquidity-container.scss";
-import React from "react";
 import { Trans, useTranslation } from "react-i18next";
 import { EthConnectPrompt } from "../../components/eth-connect-prompt";
 import { Links, REWARDS_ADDRESSES } from "../../config";
 import { DexTokensSection } from "./dex-table";
 import { LiquidityState } from "./liquidity-reducer";
 import { useWeb3 } from "../../contexts/web3-context/web3-context";
+import { Error } from "../../components/icons";
+import { Callout } from "../../components/callout";
 
 export const LiquidityContainer = ({ state }: { state: LiquidityState }) => {
   const { t } = useTranslation();
   const { ethAddress } = useWeb3();
   return (
     <section className="liquidity-container">
-      <Trans
-        i18nKey="liquidityIntro"
-        components={{
-          bold: <strong />,
-          linkToPost: (
-            // eslint-disable-next-line jsx-a11y/anchor-has-content
-            <a
-              href={Links.INCENTIVE_PROGRAM_BLOG_POST}
-              target="_blank"
-              rel="nofollow noreferrer"
-            />
-          ),
-        }}
-      />
-      <h2>{t("liquidityTitle")}</h2>
-      <ol>
-        <li>
-          {t("liquidityStep1Title")}
-          <p>{t("liquidityStep1Body")}</p>
-        </li>
-        <li>
-          {t("liquidityStep2Title")}
-          <p>{t("liquidityStep2Body")}</p>
-        </li>
-        <li>
-          {t("liquidityStep3Title")}
-          <p>{t("liquidityStep3Body")}</p>
-        </li>
-        <li>
-          {t("liquidityStep4Title")}
-          <p>{t("liquidityStep4Body")}</p>
-        </li>
-      </ol>
+      <Callout icon={<Error />} intent="error" title={t("lpEndedTitle")}>
+        <p>{t("lpEndedParagraph")}</p>
+        <p>
+          <Trans
+            i18nKey="lpDiscordPrompt"
+            components={{
+              // eslint-disable-next-line jsx-a11y/anchor-has-content
+              discordLink: <a href={Links.DISCORD} />,
+            }}
+          />
+        </p>
+      </Callout>
 
       {!ethAddress && <EthConnectPrompt />}
       <h2>{t("liquidityRewardsTitle")}</h2>
