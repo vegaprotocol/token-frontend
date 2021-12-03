2505a9665456472ac0b0194f2ff3926d297fcacf
Dexter <dexter.edwards93@gmail.com>
2021-12-03 12:05:16 +0000

    reset amount

--- src/routes/staking/staking-form.tsx
+++ src/routes/staking/staking-form.tsx
@@ -86,7 +86,10 @@ export const StakingForm = ({
   const [removeType, setRemoveType] = React.useState<RemoveType>(
     RemoveType.endOfEpoch
   );
-
+  // Clear the amount when the staking method changes
+  React.useEffect(() => {
+    setAmount("");
+  }, [action, setAmount]);
   const { data } = useNetworkParam([
     NetworkParams.VALIDATOR_DELEGATION_MIN_AMOUNT,
   ]);
