c5ee2f621cb3431b18230d543b72a2cd1c483c49
pgriscti <2323dragon@gmail.com>
2021-12-08 13:14:39 +0000

    adds not-running contents

--- src/components/vega-wallet/vega-wallet-not-running.tsx
+++ src/components/vega-wallet/vega-wallet-not-running.tsx
@@ -1,8 +1,16 @@
+import { useTranslation } from "react-i18next";
+import "./vega-wallet-not-running.scss";
 
 export const VegaWalletNotRunning = () => {
+  const { t } = useTranslation();
   return (
-    <div>todo</div>
-  )
-}
-
-
+    <div className="vega-wallet-not-running__container">
+      <div className="vega-wallet-not-running__exclamation">!</div>
+      <div className="vega-wallet-not-running__text">
+        <p>{t("noWalletDetected")}</p>
+        <p>{t("noWalletHelpText")}</p>
+        <p>{t("walletServiceNotRunning")}</p>
+      </div>
+    </div>
+  );
+};
