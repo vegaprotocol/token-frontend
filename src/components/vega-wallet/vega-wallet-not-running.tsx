f36be8544822a8bc2843a0ab2b928130aa3b775d
pgriscti <2323dragon@gmail.com>
2021-12-08 14:25:27 +0000

    adds url input

--- src/components/vega-wallet/vega-wallet-not-running.tsx
+++ src/components/vega-wallet/vega-wallet-not-running.tsx
@@ -5,11 +5,17 @@ export const VegaWalletNotRunning = () => {
   const { t } = useTranslation();
   return (
     <div className="vega-wallet-not-running__container">
-      <div className="vega-wallet-not-running__exclamation">!</div>
-      <div className="vega-wallet-not-running__text">
-        <p>{t("noWalletDetected")}</p>
-        <p>{t("noWalletHelpText")}</p>
-        <p>{t("walletServiceNotRunning")}</p>
+      <div className="vega-wallet-not-running__input">
+        <p>{t("urlLabel")}</p>
+        <input disabled={true} type="text" className="bp3-input" />
+      </div>
+      <div className="vega-wallet-not-running__text-container">
+        <div className="vega-wallet-not-running__exclamation">!</div>
+        <div className="vega-wallet-not-running__text">
+          <p>{t("noWalletDetected")}</p>
+          <p>{t("noWalletHelpText")}</p>
+          <p>{t("walletServiceNotRunning")}</p>
+        </div>
       </div>
     </div>
   );
