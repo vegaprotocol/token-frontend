f3ca4e41b80457bfec46c09d125badad7b922096
pgriscti <2323dragon@gmail.com>
2021-12-08 14:33:28 +0000

    adds wallet url

--- src/components/vega-wallet/vega-wallet-not-running.tsx
+++ src/components/vega-wallet/vega-wallet-not-running.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import { DEFAULT_WALLET_URL } from "../../lib/vega-wallet/vega-wallet-service";
 import "./vega-wallet-not-running.scss";
 
 export const VegaWalletNotRunning = () => {
@@ -7,12 +8,17 @@ export const VegaWalletNotRunning = () => {
     <div className="vega-wallet-not-running__container">
       <div className="vega-wallet-not-running__input">
         <p>{t("urlLabel")}</p>
-        <input disabled={true} type="text" className="bp3-input" />
+        <input
+          disabled={true}
+          type="text"
+          className="bp3-input"
+          value={DEFAULT_WALLET_URL}
+        />
       </div>
       <div className="vega-wallet-not-running__text-container">
         <div className="vega-wallet-not-running__exclamation">!</div>
         <div className="vega-wallet-not-running__text">
-          <p>{t("noWalletDetected")}</p>
+          <p>{t("noWalletDetected", { url: DEFAULT_WALLET_URL })}</p>
           <p>{t("noWalletHelpText")}</p>
           <p>{t("walletServiceNotRunning")}</p>
         </div>
