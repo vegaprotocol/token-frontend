cfbf288d054abadddb7f5d0bf12d7f74b1fba5a8
pgriscti <2323dragon@gmail.com>
2021-12-09 11:21:06 +0000

    add polling

--- src/components/vega-wallet/vega-wallet-container.tsx
+++ src/components/vega-wallet/vega-wallet-container.tsx
@@ -1,21 +1,31 @@
 import React from "react";
-import { AppStateActionType, useAppState } from "../../contexts/app-state/app-state-context";
-import { Errors, vegaWalletService } from "../../lib/vega-wallet/vega-wallet-service";
+import {
+  AppStateActionType,
+  useAppState,
+  VegaWalletStatus,
+} from "../../contexts/app-state/app-state-context";
+import {
+  Errors,
+  vegaWalletService,
+} from "../../lib/vega-wallet/vega-wallet-service";
 import { VegaWalletForm } from "./vega-wallet-form";
 import { VegaWalletNotRunning } from "./vega-wallet-not-running";
 
-
 export const VegaWalletFormContainer = () => {
-  const { appDispatch } = useAppState();
-  const [isVegaWalletRunning, setIsVegaWalletRunning] = React.useState(false);
+  const { appState, appDispatch } = useAppState();
+  const [isVegaWalletRunning, setIsVegaWalletRunning] = React.useState(
+    appState.vegaWalletStatus !== VegaWalletStatus.None
+  );
 
   React.useEffect(() => {
     const run = async () => {
       const [version] = await vegaWalletService.getVersion();
-      console.log("version", version);
+      console.log("Removeme!!!!!", version);
       setIsVegaWalletRunning(version !== Errors.SERVICE_UNAVAILABLE);
     };
-    run();
+    const interval = setInterval(run, 2000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return isVegaWalletRunning ? (
@@ -30,8 +40,4 @@ export const VegaWalletFormContainer = () => {
   ) : (
     <VegaWalletNotRunning />
   );
-
-}
-
-
-
+};
