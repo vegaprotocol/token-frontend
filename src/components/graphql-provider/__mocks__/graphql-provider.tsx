30b2428bef52d5fb95ae2dd53dbe8b62a7dfa08d
pgriscti <2323dragon@gmail.com>
2021-12-23 14:29:17 +0000

    removed unused imports

--- src/components/graphql-provider/__mocks__/graphql-provider.tsx
+++ src/components/graphql-provider/__mocks__/graphql-provider.tsx
@@ -10,9 +10,7 @@ import {
 } from "../../../__generated__/globalTypes";
 import { NetworkParams } from "../../../hooks/__generated__/NetworkParams";
 import { NETWORK_PARAMS_QUERY } from "../../../hooks/use-network-param";
-import { Proposal } from "../../../routes/governance/__generated__/Proposal";
 import { Proposals } from "../../../routes/governance/__generated__/Proposals";
-import { PROPOSAL_QUERY } from "../../../routes/governance/proposal-container";
 import { PROPOSALS_QUERY } from "../../../routes/governance/proposals-container";
 import {
   generateNoVotes,
