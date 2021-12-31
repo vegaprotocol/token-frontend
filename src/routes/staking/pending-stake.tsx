a4ff9a000adcd89f8b07e8033a9175bf7c1338c4
pgriscti <2323dragon@gmail.com>
2021-12-31 13:01:11 +0000

    adds styling

--- src/routes/staking/pending-stake.tsx
+++ src/routes/staking/pending-stake.tsx
@@ -1,3 +1,5 @@
+import "./pending-stake.scss";
+
 import { BigNumber } from "../../lib/bignumber";
 
 interface PendingStakeProps {
@@ -5,5 +7,13 @@ interface PendingStakeProps {
 }
 
 export const PendingStake = ({ pendingAmount }: PendingStakeProps) => {
-  return <div>pending: {pendingAmount.toString()}</div>;
+  return (
+    <div className="your-stake__container">
+      <h2>Pending Nomination</h2>
+      <p>Pending nomination for next epoch: {pendingAmount.toString()} $VEGA</p>
+      <button type="button" className="button-secondary button-secondary--dark">
+        Cancel pending epoch nomination
+      </button>
+    </div>
+  );
 };
