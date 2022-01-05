81ecc4fc6a95f631b43a4fbd6a7f8b205393d18b
Matthew Russell <mattrussell36@gmail.com>
2022-01-04 17:48:26 -0800

    add wallet connect add all connectors to object

--- src/lib/connectors.ts
+++ src/lib/connectors.ts
@@ -1,10 +1,17 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
-export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 1337],
-});
+import { InfuraUrls } from "../config";
 
-export const walletconnect = new WalletConnectConnector({
-  qrcode: true,
-});
+export const Connectors = {
+  injected: new InjectedConnector({
+    supportedChainIds: [1, 3, 1337],
+  }),
+  walletconnect: new WalletConnectConnector({
+    rpc: {
+      1: InfuraUrls["0x1"],
+      3: InfuraUrls["0x3"],
+    },
+    qrcode: true,
+  }),
+};
