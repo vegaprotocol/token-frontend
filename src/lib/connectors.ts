7deb4c2311355aa1e112ef17312ab5f8ed836091
Matthew Russell <mattrussell36@gmail.com>
2022-01-04 18:46:04 -0800

    add eager connect and listener setup

--- src/lib/connectors.ts
+++ src/lib/connectors.ts
@@ -3,15 +3,21 @@ import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
 import { InfuraUrls } from "../config";
 
+export const injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 1337],
+});
+// @ts-ignore
+window.foo = injected;
+
+export const walletconnect = new WalletConnectConnector({
+  rpc: {
+    1: InfuraUrls["0x1"],
+    3: InfuraUrls["0x3"],
+  },
+  qrcode: true,
+});
+
 export const Connectors = {
-  injected: new InjectedConnector({
-    supportedChainIds: [1, 3, 1337],
-  }),
-  walletconnect: new WalletConnectConnector({
-    rpc: {
-      1: InfuraUrls["0x1"],
-      3: InfuraUrls["0x3"],
-    },
-    qrcode: true,
-  }),
+  injected,
+  walletconnect,
 };
