d1d7722d0371ce185c8e402fc0e326c63cb28d58
Matthew Russell <mattrussell36@gmail.com>
2022-01-04 17:48:26 -0800

    add wallet connect add all connectors to object

--- src/components/eth-connect-modal/eth-connect-modal.tsx
+++ src/components/eth-connect-modal/eth-connect-modal.tsx
@@ -1,14 +1,16 @@
 import { Overlay } from "@blueprintjs/core";
+import { useTranslation } from "react-i18next";
 
 import {
   AppStateActionType,
   useAppState,
 } from "../../contexts/app-state/app-state-context";
 import { useWeb3 } from "../../hooks/use-web3";
-import { injected, walletconnect } from "../../lib/connectors";
+import { Connectors } from "../../lib/connectors";
 import { Modal } from "../modal";
 
 export const EthConnectModal = () => {
+  const { t } = useTranslation();
   const { appState, appDispatch } = useAppState();
   const { activate } = useWeb3();
   const close = () =>
@@ -25,25 +27,22 @@ export const EthConnectModal = () => {
     >
       <div className="modal modal--dark">
         <Modal>
+          <h2>Connect Ethereum wallet</h2>
           <div>
-            <button
-              type="button"
-              onClick={() => {
-                activate(injected);
-                close();
-              }}
-            >
-              Injected
-            </button>
-            <button
-              type="button"
-              onClick={() => {
-                activate(walletconnect);
-                close();
-              }}
-            >
-              Wallet Connect
-            </button>
+            {Object.entries(Connectors).map(([key, connector]) => {
+              return (
+                <button
+                  type="button"
+                  onClick={() => {
+                    activate(connector);
+                    close();
+                  }}
+                >
+                  <div>{t(`${key}.name`)}</div>
+                  <div>{t(`${key}.text`)}</div>
+                </button>
+              );
+            })}
           </div>
         </Modal>
       </div>
