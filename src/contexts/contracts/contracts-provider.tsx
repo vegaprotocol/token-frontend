82ea37a361b50a3926707a3691cc78a56833e4ad
Matthew Russell <mattrussell36@gmail.com>
2022-01-06 20:33:19 -0800

    add basic pending state to contracts provider component utilizing new features in smart-contracts-sdk

--- src/contexts/contracts/contracts-provider.tsx
+++ src/contexts/contracts/contracts-provider.tsx
@@ -1,10 +1,12 @@
 import {
+  Networks,
   VegaClaim,
   VegaErc20Bridge,
   VegaStaking,
   VegaToken,
   VegaVesting,
 } from "@vegaprotocol/smart-contracts-sdk";
+import { ethers } from "@vegaprotocol/smart-contracts-sdk/node_modules/ethers";
 import { useWeb3React } from "@web3-react/core";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import React from "react";
@@ -18,9 +20,36 @@ import { ContractsContext, ContractsContextShape } from "./contracts-context";
  * Provides Vega Ethereum contract instances to its children.
  */
 export const ContractsProvider = ({ children }: { children: JSX.Element }) => {
-  const { library, connector } = useWeb3React();
+  const { library, connector, account } = useWeb3React();
+  const [pending, setPending] = React.useState(false);
   const [contracts, setContracts] =
     React.useState<ContractsContextShape | null>(null);
+
+  React.useEffect(() => {
+    if (!contracts || !account) return;
+
+    // Bind listeners for events on staking contract
+
+    // contracts.staking.bindEventListeners(
+    //   ["Stake_Deposited", "Stake_Removed"],
+    //   [account]
+    // );
+
+    // Watch for updates to transactions
+
+    contracts.staking.watchTransactions(
+      (
+        txs: Array<{
+          tx: ethers.providers.TransactionResponse;
+          receipt: ethers.providers.TransactionReceipt;
+          pending: boolean;
+        }>
+      ) => {
+        console.log(txs);
+        setPending(txs.some((tx) => tx.pending));
+      }
+    );
+  }, [contracts, account]);
 
   React.useEffect(() => {
     let cancelled = false;
@@ -40,12 +69,7 @@ export const ContractsProvider = ({ children }: { children: JSX.Element }) => {
         const decimals = await token.decimals();
         setContracts({
           token,
-          staking: new VegaStaking(
-            library,
-            signer,
-            ADDRESSES.stakingBridge,
-            decimals
-          ),
+          staking: new VegaStaking(library, Networks.DEVNET),
           vesting: new VegaVesting(
             library,
             signer,
@@ -84,6 +108,21 @@ export const ContractsProvider = ({ children }: { children: JSX.Element }) => {
 
   return (
     <ContractsContext.Provider value={contracts}>
+      {pending ? (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            background: "red",
+            color: "blac",
+            padding: 10,
+            zIndex: 9999,
+          }}
+        >
+          PENDING
+        </div>
+      ) : null}
       {children}
     </ContractsContext.Provider>
   );
