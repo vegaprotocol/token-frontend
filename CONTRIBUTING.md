100b55c0ccb449e5b7e52467f199ae45969686cf
Dexter Edwards <dexter.edwards93@gmail.com>
2022-04-04 08:41:31 +0100

    Merge pull request #1251 from vegaprotocol/chore/1248-change-feedback-links-from-nolt-to-github-discussions
    Change feedback links from nolt to GitHub discussions in TFE

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,3 +1,3 @@
 Thanks for wanting to contribute to Vega's token front-end site.
 
-We are not currently accepting bug reports and issues from the community on this repository. If you'd like to report an issue, please visit our [Nolt board](https://vega-testnet.nolt.io) and include as much information as possible, including screenshots and Etherscan links. We really appreciate your interest in helping improve all facets of Vega!
+We are not currently accepting bug reports and issues from the community on this repository. If you'd like to report an issue, please visit our [Feedback board](https://github.com/vegaprotocol/feedback/discussions) and include as much information as possible, including screenshots and Etherscan links. We really appreciate your interest in helping improve all facets of Vega!
--- src/config/links.ts
+++ src/config/links.ts
@@ -1,13 +1,13 @@
 export const Links = {
-  WALLET_RELEASES: "https://github.com/vegaprotocol/go-wallet/releases",
+  WALLET_RELEASES: "https://github.com/vegaprotocol/vegawallet-desktop/releases",
   WALLET_GUIDE:
-    "https://docs.vega.xyz/docs/tools/vega-wallet/cli-wallet/create-wallet",
+    "https://docs.vega.xyz/docs/tools/vega-wallet",
   SUSHI_PAIRS: "https://analytics.sushi.com/pairs/",
   SUSHI_ONSEN_MENU: "https://app.sushi.com/farm",
   SUSHI_ONSEN_WHAT_IS:
     "https://docs.sushi.com/products/yield-farming/what-is-onsen",
   SUSHI_ONSEN_FAQ: "https://docs.sushi.com/faq-1/onsen-faq",
-  NOLT: "https://vega-testnet.nolt.io",
+  FEEDBACK: "https://github.com/vegaprotocol/feedback/discussions",
   GITHUB: "https://github.com/vegaprotocol/token-frontend",
   DISCORD: "https://vega.xyz/discord",
   STAKING_GUIDE:
